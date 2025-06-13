c622ea026e6fceaf5e9cba5f8369f4ad08c91203
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-06-13 22:09:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,34 @@
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.IOException;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.io.InputStreamReader;
+
+public class Main { 
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
 		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+        
+		if (N >= 3)
+			dp[3] = 1;
+		if (N >= 5)
+			dp[5] = 1;
 		
- 
-		if (N == 4 || N == 7) {
-			System.out.println(-1);
+        for (int i = 6; i <= N; i++) {
+			if (i % 5 == 0) {
+				dp[i] = dp[i - 5] + 1;
+			} else if (i % 3 == 0) {
+				dp[i] = dp[i - 3] + 1;
+			} else {
+				if (dp[i - 3] != 0 && dp[i - 5] != 0) {
+					dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+				}
+			}
 		}
-		else if (N % 5 == 0) {
-			System.out.println(N / 5);
+		
+        if (dp[N] == 0) {
+			System.out.println("-1");
+			return;
 		}
-		else if (N % 5 == 1 || N % 5 == 3) {
-			System.out.println((N / 5) + 1);
-		}
-		else if (N % 5 == 2 || N % 5 == 4) {
-			System.out.println((N / 5) + 2);
-		}
+		System.out.println(dp[N]);
 	}
 }
