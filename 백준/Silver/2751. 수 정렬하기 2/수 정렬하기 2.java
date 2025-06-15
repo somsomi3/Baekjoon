c4b6f081504e36fedd57aac99aaae25d81e3deb7
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-06-15 19:56:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 680 ms, Memory: 93720 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,33 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 빠른 입력을 위해 BufferedReader 사용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 빠른 출력을 위해 BufferedWriter 사용
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        boolean[] arr = new boolean[2000001];
 
-        int N = Integer.parseInt(br.readLine()); // 수의 개수
-        int[] arr = new int[N]; // 입력 수를 저장할 배열
+        int N = Integer.parseInt(br.readLine());
 
-        // N번 숫자 입력
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
 
-        Arrays.sort(arr); // O(N log N) 정렬 수행 (기본: Dual-Pivot QuickSort)
-
-        // 정렬된 결과 출력
-        for (int i = 0; i < N; i++) {
-            bw.write(arr[i] + "\n");
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i]) {
+                sb.append((i - 1000000)).append('\n');
+            }
         }
-
-        bw.flush(); // 버퍼 비우기
-        bw.close(); // 닫기
+        System.out.print(sb);
     }
 }
