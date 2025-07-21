9255bc093ff05228d68c5971ba19617837f6ca48
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-07-21 23:54:55 +0900

    [Silver IV] Title: 대칭 차집합, Time: 784 ms, Memory: 98344 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,21 +1,28 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // 빠른 입력을 위한 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int aSize = sc.nextInt();
-        int bSize = sc.nextInt();
+        // 첫 줄: A와 B의 원소 개수
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int aSize = Integer.parseInt(st.nextToken());
+        int bSize = Integer.parseInt(st.nextToken());
 
+        // 집합 A 입력
         Set<Integer> A = new HashSet<>();
-        Set<Integer> B = new HashSet<>();
-
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < aSize; i++) {
-            A.add(sc.nextInt());
+            A.add(Integer.parseInt(st.nextToken()));
         }
 
+        // 집합 B 입력
+        Set<Integer> B = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < bSize; i++) {
-            B.add(sc.nextInt());
+            B.add(Integer.parseInt(st.nextToken()));
         }
 
         // A - B
@@ -26,6 +33,7 @@ public class Main {
         Set<Integer> diffBA = new HashSet<>(B);
         diffBA.removeAll(A);
 
+        // 대칭 차집합의 크기
         int result = diffAB.size() + diffBA.size();
         System.out.println(result);
     }
