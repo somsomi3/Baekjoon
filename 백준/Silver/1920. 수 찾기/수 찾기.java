fa594d19afe73259b1483ff5577b80c20aba5ee3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-08-26 20:47:22 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 45304 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,49 +3,37 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 빠른 입출력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        // 배열 A 입력
+        
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);  // 이분 탐색 위해 정렬
 
-        // 정렬 (이분 탐색 전)
-        Arrays.sort(A);
-
-        // 배열 B 입력
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            if (binarySearch(A, target)) {
-                sb.append(1).append('\n');
-            } else {
-                sb.append(0).append('\n');
-            }
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (binarySearch(arr, x)) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
         }
-
         System.out.print(sb);
     }
 
-    // 이분 탐색 함수
-    private static boolean binarySearch(int[] arr, int key) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while(left <= right) {
+    static boolean binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
             int mid = (left + right) / 2;
-
-            if(arr[mid] == key) return true;
-            else if(arr[mid] < key) left = mid + 1;
+            if (arr[mid] == target) 
+                return true;
+            else if (arr[mid] < target) 
+                left = mid + 1;
             else right = mid - 1;
         }
-
         return false;
     }
 }
