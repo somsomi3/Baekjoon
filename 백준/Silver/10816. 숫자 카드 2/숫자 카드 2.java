462647669e0fa79bb048a59bec216074e4b148e3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-08-26 22:38:45 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 400 ms, Memory: 108920 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,31 +1,57 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
+    static final int OFFSET = 10_000_000;
+    static final int SIZE = 20_000_001;
+    static final InputStream in = System.in;
+    static final byte[] buf = new byte[1 << 16];
+    static int ptr = 0, len = 0;
+
     public static void main(String[] args) throws IOException {
-        final int OFFSET = 10_000_000;
-        final int SIZE = 20_000_001;
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int N = nextInt();
+        int[] count = new int[SIZE];
 
-        int N = Integer.parseInt(br.readLine());
-        int[] count = new int[SIZE]; // -10_000_000 ~ 10_000_000
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
+            int num = nextInt();
             count[num + OFFSET]++;
         }
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        int M = nextInt();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < M; i++) {
-            int query = Integer.parseInt(st.nextToken());
+            int query = nextInt();
             sb.append(count[query + OFFSET]).append(' ');
         }
 
         System.out.println(sb);
+    }
+
+    static int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buf);
+            ptr = 0;
+            if (len <= 0) return -1;
+        }
+        return buf[ptr++];
+    }
+
+    static int nextInt() throws IOException {
+        int c, sign = 1, val = 0;
+        do {
+            c = read();
+        } while (c <= 32);
+
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+
+        while (c >= '0' && c <= '9') {
+            val = (val << 3) + (val << 1) + (c & 15); // val*10 + (c-'0')
+            c = read();
+        }
+
+        return val * sign;
     }
 }
