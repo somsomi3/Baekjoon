2ced090fe0004500cdfc43856280563a6eef9194
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-08-28 16:15:01 +0900

    [Gold IV] Title: 공유기 설치, Time: 160 ms, Memory: 15028 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,24 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, C;
+    static final InputStream in = System.in;
+    static final byte[] buf = new byte[1 << 16];
+    static int ptr = 0, len = 0;
+
+    static int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buf);
+            ptr = 0;
+            if (len <= 0) return -1;
+        }
+        return buf[ptr++];
+    }
+
+    static int nextInt() throws IOException {
+        int c, val = 0;
+        do {
+            c = read();
+        } while (c <= 32); // 공백, 개행 무시
+
+        boolean neg = false;
+        if (c == '-') {
+            neg = true;
+            c = read();
+        }
+
+        while (c > 32) {
+            val = (val << 3) + (val << 1) + (c - '0'); 
+            c = read();
+        }
+        return neg ? -val : val;
+    }
+
     static int[] house;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
+        int N = nextInt();
+        int C = nextInt();
         house = new int[N];
+
         for (int i = 0; i < N; i++) {
-            house[i] = Integer.parseInt(br.readLine());
+            house[i] = nextInt();
         }
 
         Arrays.sort(house);
@@ -28,9 +52,9 @@ public class Main {
         int answer = 0;
 
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = (left + right) >> 1;
 
-            if (canInstall(mid)) {
+            if (canInstall(mid, C, N)) {
                 answer = mid;
                 left = mid + 1;
             } else {
@@ -41,12 +65,12 @@ public class Main {
         System.out.println(answer);
     }
 
-    static boolean canInstall(int dist) {
+    static boolean canInstall(int minDist, int C, int N) {
         int count = 1;
         int last = house[0];
 
         for (int i = 1; i < N; i++) {
-            if (house[i] - last >= dist) {
+            if (house[i] - last >= minDist) {
                 count++;
                 last = house[i];
             }
