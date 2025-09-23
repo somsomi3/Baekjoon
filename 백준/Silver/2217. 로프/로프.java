9bb513d55ab99bb1d9c3cf4325bcad4d8a73ea25
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-09-23 22:29:25 +0900

    [Silver IV] Title: 로프, Time: 196 ms, Memory: 23540 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,48 +1,25 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static final InputStream in = System.in;
-    static final byte[] buffer = new byte[1 << 16];
-    static int ptr = 0, len = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-    static int read() throws IOException {
-        if (ptr >= len) {
-            len = in.read(buffer);
-            ptr = 0;
-            if (len <= 0) return -1;
-        }
-        return buffer[ptr++];
-    }
-
-    static int nextInt() throws IOException {
-        int c, sign = 1, val = 0;
-        c = read();
-        while (c <= 32) {
-            if (c == -1) return -1;
-            c = read();
-        }
-        if (c == '-') {
-            sign = -1;
-            c = read();
-        }
-        while (c > 32) {
-            val = (val << 3) + (val << 1) + (c & 15);
-            c = read();
-        }
-        return val * sign;
-    }
-
-    public static void main(String[] args) throws Exception {
-        int N = nextInt();
-        arr = new int[N];
+        int MAX = 10000;
+        int[] count = new int[MAX + 1];
+        int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = nextInt();
+            int w = Integer.parseInt(br.readLine());
+            count[w]++;
         }
 
-        Arrays.sort(arr);
+        int idx = 0;
+        for (int i = 1; i <= MAX; i++) {
+            while (count[i]-- > 0) {
+                arr[idx++] = i;
+            }
+        }
 
         int maxDist = 0;
         for (int i = 0; i < N; i++) {
