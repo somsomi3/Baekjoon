66acb6f4feb371e996e3c9be7f40aeb85bceae37
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-09-23 16:37:38 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,31 +1,30 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		//동전입력받기
-		int[] coins = new int[N];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //인티저는 객체형이라 Collections.reverseOrder적용가능!
+        Integer[] coins = new Integer[N];
         for (int i = 0; i < N; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
-        
-        int count = 0;
-        
-        //그리디
-        for (int i = N - 1; i >= 0; i--) {
-        	if(coins[i]<=K) {
-        		count += K/coins[i];
-        		K%= coins[i];
-        	}
-        }
-		System.out.println(count);
-	}
 
+        Arrays.sort(coins, Collections.reverseOrder()); // 내림차순 정렬
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (coins[i] <= K) {
+                count += K / coins[i];
+                K %= coins[i];
+            }
+        }
+
+        System.out.println(count);
+    }
 }
