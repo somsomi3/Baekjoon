ecf91b1798de9bfb80b1bd5dfe56ee8350884314
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-09-23 16:41:27 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,25 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        //인티저는 객체형이라 Collections.reverseOrder적용가능!
-        Integer[] coins = new Integer[N];
-        for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(coins, Collections.reverseOrder()); // 내림차순 정렬
+        int[] coins = new int[N];
+        
+        for (int i = 0; i < N; i++) coins[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(coins); // 오름차순 정렬
 
         int count = 0;
-        for (int i = 0; i < N; i++) {
-            if (coins[i] <= K) {
-                count += K / coins[i];
-                K %= coins[i];
-            }
+        for (int i = N - 1; i >= 0 && K > 0; i--) {
+            count += K / coins[i];
+            K %= coins[i];
         }
 
         System.out.println(count);
