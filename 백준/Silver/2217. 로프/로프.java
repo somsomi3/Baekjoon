4c1afddf99a3b39c25f432a148eb97438dfe6065
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-09-23 22:08:29 +0900

    [Silver IV] Title: 로프, Time: 212 ms, Memory: 21236 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -2,26 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr;
+    static final InputStream in = System.in;
+    static final byte[] buffer = new byte[1 << 16];
+    static int ptr = 0, len = 0;
 
-		int N= Integer.parseInt(br.readLine());
-		
-		int[] rope = new int[N];
-		
-		for (int i=0 ; i<N; i++) {
-			rope[i] = Integer.parseInt(br.readLine());
-		}
-        Arrays.sort(rope); // 오름차순 정렬
-
-        int max = 0;
-        for(int i=0; i<N; i++) {
-        	int w = rope[i]*(N-i);
-        	if (w >max) {
-        		max =w;
-        	}
+    static int read() throws IOException {
+        if (ptr >= len) {
+            len = in.read(buffer);
+            ptr = 0;
+            if (len <= 0) return -1;
         }
-        System.out.println(max);
-	}
+        return buffer[ptr++];
+    }
 
+    static int nextInt() throws IOException {
+        int c, sign = 1, val = 0;
+        c = read();
+        while (c <= 32) {
+            if (c == -1) return -1;
+            c = read();
+        }
+        if (c == '-') {
+            sign = -1;
+            c = read();
+        }
+        while (c > 32) {
+            val = (val << 3) + (val << 1) + (c & 15);
+            c = read();
+        }
+        return val * sign;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int N = nextInt();
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int maxDist = 0;
+        for (int i = 0; i < N; i++) {
+            int weight = arr[i] * (N - i);
+            maxDist = Math.max(maxDist, weight);
+        }
+
+        System.out.println(maxDist);
+    }
 }
