b995ae1ccd465a74b35750cfb027ad9c3a52313f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-09-26 23:14:08 +0900

    [Gold V] Title: 집합의 표현, Time: 412 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,61 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
+    static int[] parent, size;
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+        if (size[a] < size[b]) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        parent[b] = a;
+        size[a] += size[b];
+    }
 
     public static void main(String[] args) throws IOException {
-        // 빠른 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        // 첫 줄 입력: n, m
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        // 유니온 파인드 초기화
         parent = new int[n + 1];
+        size = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
 
-        // m개의 연산 처리
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int cmd = Integer.parseInt(st.nextToken());
-            int a   = Integer.parseInt(st.nextToken());
-            int b   = Integer.parseInt(st.nextToken());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if (cmd == 0) {
-                union(a, b);
-            } else {
-                if (find(a) == find(b)) {
-                    sb.append("YES\n");
-                } else {
-                    sb.append("NO\n");
-                }
-            }
+            if (op == 0) union(a, b);
+            else sb.append(find(a) == find(b) ? "YES\n" : "NO\n");
         }
-
         System.out.print(sb);
-    }
-
-    // 루트 찾기 (경로 압축)
-    static int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-
-    // 합치기
-    static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
-        }
     }
 }
