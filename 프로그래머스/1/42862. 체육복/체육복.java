67b37d9079a34970b40273a2a70faa4cc34614e3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-10-09 21:57:00 +0900

    [level 1] Title: 체육복, Time: 0.55 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,22 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Set<Integer> lostSet = new HashSet<>();
-        Set<Integer> reserveSet = new HashSet<>();
-        
-        for (int l : lost) lostSet.add(l);
-        for (int r : reserve) reserveSet.add(r);
-        
 
-        Set<Integer> overlap = new HashSet<>(lostSet);
-        overlap.retainAll(reserveSet);
-        lostSet.removeAll(overlap);
-        reserveSet.removeAll(overlap);
-        
-        for (int r : reserveSet) {
-            if (lostSet.contains(r - 1)) lostSet.remove(r - 1);
-            else if (lostSet.contains(r + 1)) lostSet.remove(r + 1);
+        boolean[] has = new boolean[n + 1];
+        Arrays.fill(has, true);
+        for (int i : lost) has[i] = false;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        List<Integer> actualReserve = new ArrayList<>();
+        for (int r : reserve) {
+            if (!has[r]) {
+                has[r] = true; // 본인 입음
+            } else {
+                actualReserve.add(r); // 진짜 여분만 남김
+            }
         }
-        return n - lostSet.size();
+
+        for (int r : actualReserve) {
+            if (r - 1 >= 1 && !has[r - 1]) {
+                has[r - 1] = true;
+            } else if (r + 1 <= n && !has[r + 1]) {
+                has[r + 1] = true;
+            }
+        }
+
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (has[i]) count++;
+        }
+
+        return count;
     }
 }
