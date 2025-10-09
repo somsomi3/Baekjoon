4398be307182d252ea7434f1fc184d36f21f4fbe
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-10-09 22:31:34 +0900

    [level 3] Title: 입국심사, Time: 56.10 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        
+        long left = 1;
+        long right = 1_000_000_000L * n;
+        while(left<right){
+            long mid = left +(right-left)/2;
+            long total = 0;
+            for (int time : times) {
+                total += mid / time;
+            }
+            
+            if(total<n){
+                left= mid+1;
+            }else{
+                right= mid;
+            }
+        }
+        
+        return left;
+    }
+}
