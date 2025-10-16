1d9d98822cd4a4125f603b85175dd885ada70d2c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-10-16 11:46:14 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 544 ms, Memory: 55552 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,34 +1,37 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+	    for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] prefix = new int[N+1];
+		//누적합
+		for(int i =1; i<=N; i++) {
+			prefix[i] = prefix[i-1]+arr[i];
+		}
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N+1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] prefix = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            prefix[i] = prefix[i-1] + arr[i];
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int j = 0; j < M; j++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(prefix[b] - prefix[a-1]).append('\n');
-        }
-
-        System.out.print(sb);
-    }
+		StringBuilder sb = new StringBuilder();
+		
+		
+		for( int j =0; j<M; j++) {
+			st = new StringTokenizer(br.readLine());
+			int l = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			
+			sb.append(prefix[r]-prefix[l-1]).append('\n');
+			
+		}
+		System.out.print(sb);
+	}
 }
