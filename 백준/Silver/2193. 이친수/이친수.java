15e5c8f4cce9211a60e971a322bec8e32f642589
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-10-28 22:34:24 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,19 +2,18 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][2];
 
-        long[] dp = new long[N + 1]; // N이 최대 90이라서 long 써야 함!
+        dp[1][0] = 0;
+        dp[1][1] = 1;
 
-        dp[1] = 1;
-        if (N >= 2) dp[2] = 1;
-
-        for (int i = 3; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
         }
 
-        System.out.println(dp[N]);
+        System.out.println(dp[n][0] + dp[n][1]);
     }
 }
