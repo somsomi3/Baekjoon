343f62a0b63e9a750ae853a680074ffbc48c24cd
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-18 20:11:56 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String s = "강한친구 대한육군";
+		
+		System.out.println(s);
+		System.out.println(s);
+	}
+}
