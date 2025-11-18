47904ffbacafc9fd5f22bd44c24a63a2926cd388
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-18 20:27:29 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -9,17 +9,14 @@ public class Main {
 		int x = Integer.parseInt(br.readLine());
 		int y = Integer.parseInt(br.readLine());
 		
-		if(x >0 && y>0) {
-			System.out.println(1);
+		if(x >0) {
+			if(y >0)System.out.println(1);
+			else System.out.println(4);
 		}
-		if(x <0 && y>0) {
-			System.out.println(2);
+		if(x <0) {
+			if(y>0) System.out.println(2);
+			else System.out.println(3);
 		}
-		if(x <0 && y<0) {
-			System.out.println(3);
-		}
-		if(x >0 && y<0) {
-			System.out.println(4);
-		}
+
 	}
 }
