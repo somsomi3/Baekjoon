226036a0b6f4ae25d5f8cd9acae31cc751b26279
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-19 21:36:19 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             String s = br.readLine();
-            Stack<Character> stack = new Stack<>();
+            int cnt = 0;
             boolean ok = true;
 
-            for (char ch : s.toCharArray()) {
-                if (ch == '(') {
-                    stack.push(ch);
-                } else {
-                    if (stack.isEmpty()) {
-                        ok = false;
-                        break;
-                    }
-                    stack.pop();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+
+                if (ch == '(') cnt++;
+                else cnt--;
+
+                if (cnt < 0) { 
+                    ok = false;
+                    break;
                 }
             }
 
-            if (!stack.isEmpty()) ok = false;
+            if (cnt != 0) ok = false;
 
             sb.append(ok ? "YES\n" : "NO\n");
         }
