1d9a289a10ae326e61af9278846bf66b0c4137d2
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-19 09:34:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 448 ms, Memory: 88260 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -2,32 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N= Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-//		Arrays.sort(arr);
-//		System.out.println(arr[0]+ " " + arr[N-1]);
-		
-		int big = -1_000_001;
-		int small = 1_000_001;
-		for(int i =0; i<N; i++) {
-			if(big<arr[i]) {
-				big = arr[i];
-//				System.out.println(big);
-//			}else if(small>arr[i]) {
-			}if(small>arr[i]) {
-				small = arr[i];
-			}
-		}
-		System.out.println(small+" "+big);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            if (x < min) min = x;
+            if (x > max) max = x;
+        }
+
+        System.out.println(min + " " + max);
+    }
 }
