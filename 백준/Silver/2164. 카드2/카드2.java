ee9319b1d8c71b244ebb7ac67c374a65417b7533
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-20 21:34:51 +0900

    [Silver IV] Title: 카드2, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,21 +1,13 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		Deque<Integer> q = new ArrayDeque<>();
-		
-		for(int i =1; i<=N; i++) {
-			q.addLast(i);
-		}
-		while(q.size()>1) {
-			q.pollFirst();
-			q.addLast(q.pollFirst());
-		}
-		System.out.println(q.pop());
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int L = Integer.highestOneBit(N);  // N 이하 최대 2의 거듭제곱
+
+        if (N == L) System.out.println(N);
+        else System.out.println((N - L) << 1);  // 2 * (N - L)을 비트연산으로
+    }
 }
