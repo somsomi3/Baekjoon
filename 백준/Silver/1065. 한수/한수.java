7904001006973fc195f12b76aa15bb6eab84a4d2
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-20 17:57:02 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,34 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();  // 입력값
-        
-        System.out.println(han(N));  // 결과 출력
-    }
-
-    public static int han(int N) {
-        if (N < 100) {
-            // 1~99는 모두 한수
-            return N;
-        }
-
-        int count = 99; // 1~99는 모두 한수라서 기본 99로 시작
-
-        for (int i = 100; i <= N; i++) {
-            int hun = i / 100;          // 백의 자리
-            int ten = (i / 10) % 10;    // 십의 자리
-            int one = i % 10;           // 일의 자리
-
-            if ((hun - ten) == (ten - one)) {
-                // 등차수열 만족하면 한수!
-                count++;
-            }
-        }
-
-        return count;
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int count =0;
+		
+		for(int i =1; i<=N; i++) {
+			if(i < 100) count++;
+			else {
+				if(i/100- (i/10)%10== (i/10)%10- i%10) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
 }
