d7cdd25b0ab21d1495e9de06db8b33ab16ecb436
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-20 20:48:48 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 22268 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int K = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < K; i++) {
-            int num = Integer.parseInt(br.readLine());
-            
-            if (num == 0) {
-                if (!stack.isEmpty()) {
-                    stack.pop(); // 최근 숫자 제거
-                }
+        int[] stack = new int[N];
+        int top = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) {
+                stack[top++] = x;
             } else {
-                stack.push(num); // 스택에 추가
+                top--;
             }
         }
 
-        int sum = 0;
-        for (int n : stack) {
-            sum += n; // 스택에 남아있는 수 모두 더하기
+        long sum = 0;
+        for (int i = 0; i < top; i++) {
+            sum += stack[i];
         }
 
         System.out.println(sum);
