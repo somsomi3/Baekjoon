a8b51a6bcac47b9066ea5d9d469bfe40b84f0af8
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-21 11:40:47 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 1;
+		
+		for(int i=1; i<=N; i++) {
+			sum = sum*i;
+		}
+		System.out.println(sum);
+	}
+}
