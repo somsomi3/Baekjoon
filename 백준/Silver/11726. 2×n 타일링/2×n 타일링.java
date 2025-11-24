fa53b5f557291bf23b81af5def5d13e8ef013d14
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-24 21:59:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,22 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int MOD = 10007;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-
-        dp[0] = 1; // 편의상 0일 때도 1로 둔다.
-        dp[1] = 1;
-
-        for (int i = 2; i <= N; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-        }
-
-        System.out.println(dp[N]);
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N+1];
+		
+		dp[1] =1;
+        if (N >= 2) dp[2] = 2;
+		
+		for(int i= 3; i<=N; i++) {
+			dp[i]= (dp[i-1]+dp[i-2])% 10007;
+		}
+		System.out.println(dp[N]);	
+	}
 }
