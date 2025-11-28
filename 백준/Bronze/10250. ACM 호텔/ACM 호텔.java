909736060b9ad16151cbb92bdfd577f9fd2a0de2
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-28 16:50:26 +0900

    [Bronze III] Title: ACM 호텔, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -2,37 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		while(T-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int H = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-			int N = Integer.parseInt(st.nextToken());
-			
-			int x = N/H +1;
-			
-			int y = N%H;
-
-            if (y == 0) {
-                y = H;
-                x = N / H;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            st.nextToken(); // W 무시
+            int N = Integer.parseInt(st.nextToken());
             
-			String s;
-			if(x<10) {
-				s = "0"+x;
-				
-				System.out.print(y);
-				System.out.println(s);
-			}else {
-				System.out.print(y);
-				System.out.println(x);
-			}
-		}	
-	}
+            int floor = (N - 1) % H + 1;
+            int room  = (N - 1) / H + 1;
+            
+            sb.append(floor * 100 + room).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
 }
