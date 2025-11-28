dbed8dd35a4dbf0f7c905032006ef2403569ef6f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-28 22:40:52 +0900

    [Bronze II] Title: 손익분기점, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.java
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -2,25 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		int y = C-B;
-		
-		int x = 1;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
         if (C <= B) {
             System.out.println(-1);
             return;
         }
-		while(y*x -A<=0) {
-			x+=1;
-		}
-		System.out.println(x);
-	}
+
+        System.out.println(A / (C - B) + 1);
+    }
 }
