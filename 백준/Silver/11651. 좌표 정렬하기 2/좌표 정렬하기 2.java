02f0f21ef1ab570fadd345ea4f9957bfd2dc2db1
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-29 16:24:54 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 600 ms, Memory: 51660 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,26 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] points = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            points[i][0] = Integer.parseInt(st.nextToken()); // x
-            points[i][1] = Integer.parseInt(st.nextToken()); // y
-        }
-
-        Arrays.sort(points, (a, b) -> {
-            if (a[1] != b[1]) return a[1] - b[1]; // y 기준
-            return a[0] - b[0]; // 같은 y면 x 기준
-        });
-
-        StringBuilder sb = new StringBuilder();
-        for (int[] p : points) {
-            sb.append(p[0]).append(" ").append(p[1]).append("\n");
-        }
-        System.out.print(sb);
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+//		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			
+		}
+		Arrays.sort(arr, (a, b) ->{
+			if(a[1]==b[1])return a[0] -b[0];
+			return a[1]-b[1];
+		});
+		
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<N; i++) {
+			sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
