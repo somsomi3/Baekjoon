47e18b28270dfa5489a8c68d540b037932680316
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-11-30 21:09:32 +0900

    [Silver IV] Title: 대칭 차집합, Time: 696 ms, Memory: 88496 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -3,38 +3,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 빠른 입력을 위한 BufferedReader 사용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 첫 줄: A와 B의 원소 개수
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int aSize = Integer.parseInt(st.nextToken());
-        int bSize = Integer.parseInt(st.nextToken());
 
-        // 집합 A 입력
-        Set<Integer> A = new HashSet<>();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        Set<Integer> aSet = new HashSet<>();
+        Set<Integer> bSet = new HashSet<>();
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < aSize; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < A; i++) {
+            aSet.add(Integer.parseInt(st.nextToken()));
         }
 
-        // 집합 B 입력
-        Set<Integer> B = new HashSet<>();
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < bSize; i++) {
-            B.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < B; i++) {
+            bSet.add(Integer.parseInt(st.nextToken()));
         }
 
-        // A - B
-        Set<Integer> diffAB = new HashSet<>(A);
-        diffAB.removeAll(B);
+        int count = 0;
 
-        // B - A
-        Set<Integer> diffBA = new HashSet<>(B);
-        diffBA.removeAll(A);
+        for (int x : aSet) {
+            if (!bSet.contains(x)) count++;
+        }
+        for (int y : bSet) {
+            if (!aSet.contains(y)) count++;
+        }
 
-        // 대칭 차집합의 크기
-        int result = diffAB.size() + diffBA.size();
-        System.out.println(result);
+        System.out.println(count);
     }
 }
