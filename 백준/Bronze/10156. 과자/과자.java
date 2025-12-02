dec6bf6dd6f6554fc88008b0747ff92ff4b9ea7c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-02 23:58:08 +0900

    [Bronze IV] Title: 과자, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int need = K * N;
+        int result = need - M;
+
+        if (result < 0) result = 0;
+
+        System.out.println(result);
+    }
+}
