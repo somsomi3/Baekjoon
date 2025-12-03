ccee454b29e5e4abe82d717b243f6f248cb3087d
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-03 22:59:04 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1088 ms, Memory: 59920 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashMap<String, String> map = new HashMap<>();
+		
+		for(int i =0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String juso = st.nextToken();
+			String password = st.nextToken();
+			map.put(juso, password);
+		}
+		
+		for(int j =0; j<M; j++) {
+			String qq = br.readLine();
+			System.out.println(map.get(qq));
+		}
+	}
+}
