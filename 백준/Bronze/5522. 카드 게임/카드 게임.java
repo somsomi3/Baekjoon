6aa39bf911edf392079d55c6afe442c1dd60d975
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-03 20:31:36 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 14148 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = 5;
+		
+		int sum = 0;
+		for(int i=0; i<N; i++) {
+			sum+=Integer.parseInt(br.readLine());
+		}
+		
+		System.out.println(sum);
+	}
+}
