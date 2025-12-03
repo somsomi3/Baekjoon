5919686ddaffaca8d1fabdf58d6f2775dc5e6845
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-03 22:26:24 +0900

    [Silver V] Title: 뒤집기, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,28 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String s= br.readLine();
-		
-		int count1 = 0;
-		int count0 = 0;
-		
-		
-		if(s.charAt(0)-'0'==0)count0++;
-		if(s.charAt(0)-'0'==1)count1++;		
-		
-		for(int i=1; i<s.length(); i++) {
-			int x = s.charAt(i)-'0';
-			
-			if(s.charAt(i)-'0'!=s.charAt(i-1)-'0' && s.charAt(i)-'0'==1 ) {
-				count1++;
-			}else if(s.charAt(i)-'0'!=s.charAt(i-1)-'0' && s.charAt(i)-'0'==0) {
-				count0++;
-			}
-		}
-		System.out.println(Math.min(count1, count0));
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int c0 = 0;
+        int c1 = 0;
+
+        char prev = s.charAt(0);
+        if (prev == '0') c0++;
+        else c1++;
+
+        for (int i = 1; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur != prev) {
+                if (cur == '0') c0++;
+                else c1++;
+                prev = cur;
+            }
+        }
+
+        System.out.println(Math.min(c0, c1));
+    }
 }
