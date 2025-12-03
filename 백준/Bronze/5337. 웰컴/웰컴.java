0c76349121c565d73f2850f223aa99beb2488541
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-03 20:45:42 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
