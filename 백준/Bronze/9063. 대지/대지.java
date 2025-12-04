da35cfb7e8f5f09d948bcff519d4fd2ad504be30
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-04 21:04:48 +0900

    [Bronze III] Title: 대지, Time: 240 ms, Memory: 27224 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -2,30 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		int[] brr = new int[N];
-		
-		int mina = 10001;
-		int maxa = -10001;
-		int minb = 10001;
-		int maxb = -10001;
-		
-		for(int i =0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i]  = Integer.parseInt(st.nextToken());
-			if(arr[i]<mina)mina =arr[i];
-			if(arr[i]>maxa)maxa =arr[i];
-			brr[i] = Integer.parseInt(st.nextToken());
-			if(brr[i]<minb)minb = brr[i];
-			if(brr[i]>maxb)maxb = brr[i];
-		}
-		
-		System.out.println((maxa-mina)*(maxb-minb));
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int minX = 10001, maxX = -10001;
+        int minY = 10001, maxY = -10001;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
 }
