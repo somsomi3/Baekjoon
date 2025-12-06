20fb513b50b384593ef696a117ea5ab24eb6a15b
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-06 12:36:17 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+//주의할점: \은 두개를 입력해야 하나를출력할 수 있다
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
