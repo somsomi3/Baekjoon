1c00fb559b27c2f0b8a7ea96dc7ea9ea97d21513
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-06 20:58:58 +0900

    [Gold IV] Title: 회전 초밥, Time: 448 ms, Memory: 170676 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -2,40 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());//초밥의 가짓수
+		int k = Integer.parseInt(st.nextToken());//연속해서 먹는 접시수
+		int c = Integer.parseInt(st.nextToken());//쿠폰번호
 
         int[] arr = new int[N];
-        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(br.readLine());
+		
+		int[] count = new int[d+1];// 초밥은 1번부터 시작인듯
+		
+		int cnt = 0;
 
-        int[] cnt = new int[d + 1];
-        int distinct = 0;
-
-        for (int i = 0; i < k; i++) {
-            if (cnt[arr[i]] == 0) distinct++;
-            cnt[arr[i]]++;
+		//접시 갯수 k만큼 채워보자
+        for(int i =0; i<k; i++) {
+            if(count[arr[i]] == 0) {
+                cnt++;
+            }
+            count[arr[i]]++;
         }
+        int result = cnt;
+        if(count[c] == 0) result++;
+		for(int i=1; i<N; i++) {
+            int remove = arr[i-1];
+            count[remove]--;
+            if(count[remove] == 0) cnt--;
 
-        int ans = distinct + (cnt[c] == 0 ? 1 : 0);
+            int add = arr[(i+k-1) % N];
+            if(count[add] == 0) cnt++;
+            count[add]++;
 
-        for (int i = 1; i < N; i++) {
-            int remove = arr[i - 1];
-            cnt[remove]--;
-            if (cnt[remove] == 0) distinct--;
+            int temp = cnt;
+            if(count[c] == 0) temp++;
 
-            int add = arr[(i + k - 1) % N];
-            if (cnt[add] == 0) distinct++;
-            cnt[add]++;
-
-            ans = Math.max(ans, distinct + (cnt[c] == 0 ? 1 : 0));
-        }
-
-        System.out.println(ans);
+            result = Math.max(result, temp);
+		}
+        System.out.println(result);
     }
 }
