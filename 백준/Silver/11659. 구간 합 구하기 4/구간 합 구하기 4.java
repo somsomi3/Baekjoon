2649cf4b09acd4d375642c8e8b5b726308011161
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-07 21:31:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 488 ms, Memory: 53944 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -9,29 +9,31 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-	    for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		// 누적합에서는 항상 1부터 입력 받자. 그래야 깔끔하다.
+		//그래서 항상 배열을 정의할때는 [N+1]
 		
-		int[] prefix = new int[N+1];
-		//누적합
-		for(int i =1; i<=N; i++) {
-			prefix[i] = prefix[i-1]+arr[i];
+		int[] arr = new int[N+1];
+		int[] prefix= new int[N+1];//prefix: 압부분, 압부분의 합
+		
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			prefix[i]= prefix[i-1]+arr[i];
 		}
-
 		StringBuilder sb = new StringBuilder();
 		
-		
-		for( int j =0; j<M; j++) {
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int l = Integer.parseInt(st.nextToken());
 			int r = Integer.parseInt(st.nextToken());
 			
-			sb.append(prefix[r]-prefix[l-1]).append('\n');
+			int x = prefix[r]-prefix[l-1];
+			sb.append(x).append('\n');
+//			System.out.println(x);
 			
 		}
+
 		System.out.print(sb);
 	}
 }
