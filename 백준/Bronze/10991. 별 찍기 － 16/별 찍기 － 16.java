7c094d0c9e0a856fe8bc0b88f5da6c97476bc4a3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-08 11:45:58 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++) {
+			for(int j=N-i; j>0; j--) {
+				sb.append(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				if(j!=i) {
+					sb.append("*").append(" ");
+				}
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+		
+	}
+}
