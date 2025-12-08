76d693fd11e5eddd216161dc64c22636f2711a57
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-08 11:28:33 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 92 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
+		
+		int x = a+b+c+d;
+		
+		System.out.println(x/60);
+		System.out.println(x%60);
+		
+	}
+}
