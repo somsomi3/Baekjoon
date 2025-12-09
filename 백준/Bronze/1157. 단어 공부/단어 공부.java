62e5534d6c65b678d0b3a736dc706cc0acb75d4e
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-09 15:46:51 +0900

    [Bronze I] Title: 단어 공부, Time: 236 ms, Memory: 20184 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,30 +1,36 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine().toUpperCase();
 		
-		int[] arr = new int[26];
+		String s = br.readLine();
+		int[] cnt = new int[26];
+		
 		for(int i =0; i<s.length(); i++) {
-			char c = s.charAt(i);
-			arr[c-'A']++;
-			
+			int x = Character.toUpperCase(s.charAt(i)) - 'A';
+			cnt[x]++;
 		}
-        int max = -1;
-        char result = '?';
-        
-        for(int i =0; i<26; i++) {
-        	if(arr[i]>max) {
-        		max = arr[i];
-                result = (char)(i + 'A'); 
-        	}else if (arr[i]==max) {
-        		   result = '?';
-            }
-        }
-
-        System.out.println(result);
-    }
+		
+		int max =0;
+		int bigans=0;
+		boolean flag = true;
+		for(int i =0; i<26;i++) {
+			if(cnt[i]>max) {
+				max=cnt[i];
+				bigans =i;
+				
+			}
+		}
+		//밖에서 또 for을 돌려서 확인해야함. 그래야 max값이 몇개인지 찾을 수 있다.
+		int maxVs = 0;
+		for(int i =0; i<26;i++) {
+			if(cnt[i]==max) {
+				maxVs++;
+			}
+		}
+		if(maxVs>1)System.out.println("?");
+		else System.out.println((char)(bigans+'A'));
+	}
 }
