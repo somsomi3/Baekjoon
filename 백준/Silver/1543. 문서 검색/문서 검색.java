f10614f9e7616870502a0352ca0c5d8989d770b9
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-09 22:25:30 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -2,21 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s = br.readLine();
         String t = br.readLine();
+        int sl = s.length();
+        int tl = t.length();
 
         int count = 0;
-        int i = 0;
 
-        while (i + t.length() <= s.length()) {
-            if (s.substring(i, i + t.length()).equals(t)) {
+        for (int i = 0; i <= sl - tl; ) {
+            boolean match = true;
+
+            for (int j = 0; j < tl; j++) {
+                if (s.charAt(i + j) != t.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
                 count++;
-                i += t.length();  // 겹치지 않게
+                i += tl;     // 겹치면 안 되므로 t 길이만큼 점프
             } else {
-                i++;  // 다음 문자로 이동
+                i++;
             }
         }
 
