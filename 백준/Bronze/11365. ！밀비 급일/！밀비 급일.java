1c98946d94a29208fbab9e0c6ce9aace221bde08
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-12 10:20:11 +0900

    [Bronze IV] Title: !밀비 급일, Time: 104 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while(true) {
+			String s = br.readLine();
+
+			if (s.equals("END")) break;	
+			StringBuilder sb = new StringBuilder(s);
+			System.out.println(sb.reverse());	
+		}
+	}
+}
