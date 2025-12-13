78d64edb6b6abbe2f2793f8001d65502fcc65b59
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-13 18:16:17 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 132 ms, Memory: 16032 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String s = br.readLine();
+		
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+s+"::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
