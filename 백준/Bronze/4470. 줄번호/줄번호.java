f0daa45de0bbc5299eec29e27228a1fb5db6b43c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-15 14:53:18 +0900

    [Bronze IV] Title: 줄번호, Time: 124 ms, Memory: 16044 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			String s = br.readLine();
+			System.out.println(i+". "+s);
+		}
+		
+	}
+}
