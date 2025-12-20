71b2d588040a91f0b30e7c4479e2389880a29575
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-20 10:48:14 +0900

    [Bronze IV] Title: 팀 나누기, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		
+		int x = Math.abs(A+D-(B+C));
+		
+		System.out.println(x);
+	}
+}
