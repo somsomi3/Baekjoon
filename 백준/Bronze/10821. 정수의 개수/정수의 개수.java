044d56362e9268365af760b277e6824f1a67d8c8
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-20 11:48:39 +0900

    [Bronze II] Title: 정수의 개수, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		int count =1;
+		for(int i =0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			if (c==',')count++; 
+		}
+		System.out.println(count);
+	}
+}
