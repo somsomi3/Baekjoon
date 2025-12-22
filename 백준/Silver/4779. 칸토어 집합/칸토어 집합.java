cde3163907de52215332a247edc65f8e7b6ae234
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-22 20:15:19 +0900

    [Silver III] Title: 칸토어 집합, Time: 188 ms, Memory: 18976 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,42 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static char[] arr;
-	static void cant(int start, int len) {
-		if(len ==1)return;
-		int third = len/3;
-		
-        for (int i = start + third; i < start + 2 * third; i++) {
-            arr[i] = ' ';
-        }
-		cant(start,third);
-        cant(start + 2 * third, third);// 마지막 third는 end값이 아니라, len이라서 길이 입니다^^
-	}
-	
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String line;
-		
-		while((line = br.readLine())!=null) {
-		    int N = Integer.parseInt(line);
-		    
-			int len  = (int )Math.pow(3, N);
-			//Math.pow의 반환타입은 double이다.
-			
-			arr= new char[len];
-			for(int i=0; i<len; i++) {
-				arr[i] = '-';
-			}
-			
-			//인쇄를 while안에서 하기
-			cant(0, len);
-			
-			//출력
 
-			System.out.println(new String(arr));
-		}
-		
-	}
+    static char[] line;
+
+    static void cant(int start, int len) {
+        if (len == 1) return;
+
+        int third = len / 3;
+
+        for (int i = start + third; i < start + 2 * third; i++) {
+            line[i] = ' ';
+        }
+
+        if (third > 1) {
+            cant(start, third);
+            cant(start + 2 * third, third);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        while ((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input);
+            int len = (int) Math.pow(3, N);
+
+            line = new char[len];
+            for (int i = 0; i < len; i++) {
+                line[i] = '-';
+            }
+
+            cant(0, len);
+
+            System.out.println(line);
+        }
+    }
 }
