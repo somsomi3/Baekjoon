dd99246c1ddcf57562300fb87eaab2d3cef105b3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-26 10:39:57 +0900

    [Bronze II] Title: 시그마, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2355. 시그마/시그마.java
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -1,17 +1,21 @@
 import java.io.*;
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		long A= Long.parseLong(st.nextToken());
-		long B= Long.parseLong(st.nextToken());
-		
-		long n = Math.abs(B-A)+1;
-		long sum =(A+B)*n/2;
-		
-		System.out.println(sum);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        BigInteger min = A.min(B);
+        BigInteger max = A.max(B);
+
+        BigInteger n = max.subtract(min).add(BigInteger.ONE);
+        BigInteger sum = min.add(max).multiply(n).divide(BigInteger.valueOf(2));
+
+        System.out.println(sum);
+    }
 }
