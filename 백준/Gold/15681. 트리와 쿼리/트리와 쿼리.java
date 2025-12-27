a60fcbc9484e7fc673dea01a6f3a1e698d1dbe89
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-27 21:53:14 +0900

    [Gold V] Title: 트리와 쿼리, Time: 504 ms, Memory: 77956 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,57 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
 	static List<Integer>[] graph;
 	static boolean[] visited;
-	static int N, R, Q;
-	static int[] familly;
-	static int count;
+//	static int[] parent;
+	static int[] subtree;
+	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		Q = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int Q = Integer.parseInt(st.nextToken());
 		
-		//그래프 만들기, 그래프는 정점 1부터 시작하므로, i=1부터 시작함.
+		//그래프만들기
 		graph = new ArrayList[N+1];
-		for(int i =1; i<=N; i++) graph[i]= new ArrayList<>();
+		for(int i =1; i<=N; i++)graph[i] = new ArrayList<>();
 		
-		familly = new int[N+1];
 		visited = new boolean[N+1];
+		subtree = new int[N+1];
 		
-		for(int i=0; i<N-1; i++){
+		for(int i =0; i<N-1; i++) {
 			st = new StringTokenizer(br.readLine());
-			//간선은 정점N 보다 1개 작다.
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 			
-			//트리는 무방향= 양방향
-			graph[a].add(b);
-			graph[b].add(a);
+			graph[u].add(v);
+			graph[v].add(u);
 		}
 		
-		//트리 돌리기1
 		dfs(R);
-		
-        StringBuilder sb = new StringBuilder();  
-        for (int i = 0; i < Q; i++) {
-            //뽑아쓰기2
-        	int x = Integer.parseInt(br.readLine());
-            sb.append(familly[x]).append('\n');
-        }
+		//for(int i= 0; i<Q; i++) {
+		//	int x  = Integer.parseInt(br.readLine());
+		//	System.out.println(subtree[x]);
+		//}
+        // 최적화
+		StringBuilder sb = new StringBuilder();
+		for(int i =0; i<Q; i++) {
+			int x = Integer.parseInt(br.readLine());
+			sb.append(subtree[x]).append('\n');
+		}
         System.out.print(sb);
 	}
-	
-	//다 구해놓고1
 	static void dfs(int cur) {
-		visited[cur]= true;
-		familly[cur] = 1;
-		for(int next: graph[cur]) {
-            if (!visited[next]) {
-                dfs(next);
-                familly[cur] += familly[next];
+		visited[cur] = true;
+		//
+	    subtree[cur] = 1;
+		
+		for(int nxt : graph[cur]) {
+			if(!visited[nxt]) {
+//				parent[cur]++;
+				dfs(nxt);
+				subtree[cur] += subtree[nxt];//자식 값을 더해준다.
 			}
 		}
 	}
