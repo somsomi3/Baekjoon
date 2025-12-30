07cc07c6a57eba9364586f873872a4ae61242345
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-30 10:05:31 +0900

    [Bronze III] Title: 거북이, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[4];
+		for(int i = 0; i<4; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+		}
+		Arrays.sort(arr);
+		
+		int x = arr[0]*arr[2];
+		System.out.println(x);
+	}
+	
+}
