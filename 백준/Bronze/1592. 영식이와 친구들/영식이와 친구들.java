4e3de8fc11058e812fbd57b44296c6b75ffb6d7c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2025-12-30 13:54:45 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 96 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -2,27 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); 
-		int M = Integer.parseInt(st.nextToken());
-		int L= Integer.parseInt(st.nextToken());
-		
-		int g = gcd(N, L);
-		
-		int x = N*(M-1)/g;
-		System.out.println(x);
-		
-	}
-	
-	static int gcd(int n, int l) {
-		while(l>0) {
-			int r = n%l;
-			n = l;
-			l= r;
-		}
-		return n;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        //카운팅 배열
+        int[] cnt = new int[N + 1];
+ 
+        //1번 부터 시작
+        int cur = 1;
+        
+        //1번에 1을 넣고 1번부터 시작
+        cnt[1] = 1;
+        
+        int ans = 0;
+
+        //cur을 잡고, cnt안의 cur을 이동 시키는 게 핵심이다.// 내가 했던for문은 cur을 무조건 i++해서 다 증가 시키는 건데, 여긴
+        //while을 사용함으로 써, while문 안에서 cur을 자유롭게 수정한다.
+        while (cnt[cur] < M) {//현재cur의 cnt가 <M인경우만 while안에서 계산을 한다.
+            if (cnt[cur] % 2 == 1) {
+                cur += L;//1-index라서 cnt에 +-N을 사용한다. 0-index라면 %N을 사용하는 것이다.
+                if (cur > N) cur -= N;
+            } else {
+                cur -= L;
+                if (cur <= 0) cur += N;
+            }
+            cnt[cur]++;
+            ans++;
+        }
+
+        System.out.println(ans);
+    }
 }
