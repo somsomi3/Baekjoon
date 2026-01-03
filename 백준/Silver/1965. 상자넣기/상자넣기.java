79f23bb1f998e168acdbeaa55b3b01d087e64cd0
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-03 19:55:56 +0900

    [Silver II] Title: 상자넣기, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -5,29 +5,31 @@ public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		int[] dp = new int[N];
+		int N= Integer.parseInt(br.readLine());
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
+		int[] arr= new int[N];
+		int[] dp = new int[N];
+
+		
+		for(int i =0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-
-		for(int i = 0; i<N; i++) {
-//			dp[1]= 1;
-			dp[i] = 1;
-			for(int j= 0; j<i; j++) {
-				if(arr[j]<arr[i]) {
-					dp[i] = Math.max(dp[i], dp[j]+1);
-				}
-			}
-		}
 		
-		int max = 0;
-		for(int i = 0; i<N; i++) {
+		dp[0]= 1;
+		int max = 1;
+		
+		for (int i = 0; i < N; i++) {
+		    dp[i] = 1;
+		    for (int j = 0; j < i; j++) {
+		        if (arr[j] < arr[i]) {// 여기가 핵심. 이중 for문 생각은 햇음.
+		            dp[i] = Math.max(dp[i], dp[j] + 1);
+		        }
+		    }
+		}
+
+		
+		for(int i=0; i<N; i++) {
 			max = Math.max(max, dp[i]);
 		}
 		
