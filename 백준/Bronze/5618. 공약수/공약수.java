e0747cb5ee2b0bf659763209e1e4b28adafcabf3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-05 10:34:50 +0900

    [Bronze II] Title: 공약수, Time: 448 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -1,37 +1,35 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int gcd(int a, int b) {
+		while(b>0) {
+			int r = a%b;
+			a=b;
+			b=r;
+		}
+		return a;
+	}
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] arr = new int[N];
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
 		
-		int[] count = new int[arr[0]+1];
-		
-		for(int x : arr) {
-			int max =0;
-			 for(int i =1; i<=arr[0]; i++) {
-				 if(x%i==0) {
-					 count[i]++;
-				 }
-			 }
+		int g= arr[0];
+		for(int i =1; i<N; i++) {
+			g = gcd(g, arr[i]);
 		}
-		for(int i =1; i<=arr[0]; i++) {
-			if(count[i]==N) {
-				System.out.println(i);
+		for(int j=1; j<=g; j++) {
+		if(g%j==0) {
+			System.out.println(j);
 			}
 		}
 	}
-	
 }
