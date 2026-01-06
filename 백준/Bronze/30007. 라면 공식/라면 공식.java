e2476b756073f49fcf2283c2f5f6b1a891ccaaf4
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-06 10:48:35 +0900

    [Bronze V] Title: 라면 공식, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N= Integer.parseInt(br.readLine());
+		
+		for(int i =0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			
+			int W = A*(X-1)+B;
+			
+			System.out.println(W);
+		}
+	}
+}
