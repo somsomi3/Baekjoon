f731e4cd430b5417b4ea6815d11c68d61ac203e3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-07 10:10:11 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		if(M==1||M==2)System.out.println("NEWBIE!");
+		else if(M!=1&& M!=2 && M<=N)System.out.println("OLDBIE!");
+		else System.out.println("TLE!");
+	}
+}
