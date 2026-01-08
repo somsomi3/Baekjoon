3ab7f54fee6ee3207a2d06f8780de2e9412f1fbb
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-08 22:58:24 +0900

    [Silver II] Title: 예산, Time: 152 ms, Memory: 15612 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -2,46 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static int S;
+	static int N, M;
+	static int[] arr;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int sum =0;
+		for(int i =0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum+= arr[i];
+		}
+		M =Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        S = Integer.parseInt(br.readLine());
-
-        Arrays.sort(arr);
-
-        int left = 0;
-        int right = arr[N - 1];
-        int answer = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (isPossible(mid)) {
-                answer = mid;
-                left = mid + 1; // 상한액을 더 키워보기
-            } else {
-                right = mid - 1; // 상한액 줄이기
-            }
-        }
-
-        System.out.println(answer);
-    }
-
-    static boolean isPossible(int mid) {
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += Math.min(arr[i], mid); // 상한액 적용
-        }
-        return sum <= S;
-    }
+		if(sum<=M)System.out.println(arr[N-1]);
+		
+		else {
+			int left =0;
+			int right = arr[N-1];
+			int ans = 0;
+			
+			while(left<=right) {
+				int mid = (left+right)/2;
+				if (canMoney(mid) <= M){
+					ans = mid;
+					left = mid+1;
+				}else {
+					right = mid-1;
+				}
+			}System.out.println(ans);
+		}
+		
+		
+	}
+	static int canMoney(int mid) {
+		int canSum =0;
+		for(int i=0; i<N; i++) {
+			if(arr[i]<=mid)canSum+=arr[i];
+			else canSum+=mid;
+		}
+		return canSum;
+	}
 }
