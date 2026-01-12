1806cea7f246dd5a453078d2540db01ad339cf59
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-12 22:18:41 +0900

    [Silver II] Title: 로또, Time: 120 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,44 +1,44 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int k;
-    static int[] arr;
-    static boolean[] visited;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            k = sc.nextInt();
-            if (k == 0) break;
-
-            arr = new int[k];
-            visited = new boolean[k];
-
-            for (int i = 0; i < k; i++) {
-                arr[i] = sc.nextInt();
-            }
-
-            dfs(0, 0);
-            System.out.println();
-        }
-    }
-
-    static void dfs(int start, int depth) {
-        if (depth == 6) {
-            for (int i = 0; i < k; i++) {
-                if (visited[i]) {
-                    System.out.print(arr[i] + " ");
-                }
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = start; i < k; i++) {
-            visited[i] = true;
-            dfs(i + 1, depth + 1);
-            visited[i] = false;
-        }
-    }
+	static int[] arr;
+	static int[] ans = new int[6];
+	static int N;
+    static StringBuilder sb = new StringBuilder();
+    
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		while(true) {
+			String line = br.readLine();
+			if(line.equals("0"))break;
+			
+			StringTokenizer st = new StringTokenizer(line);
+			N = Integer.parseInt(st.nextToken());
+			
+			arr = new int[N];
+			for(int i =0; i<N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			dfs(0,0);
+			sb.append("\n");
+		}
+        System.out.print(sb);
+	}
+	static void dfs(int depth, int start) {
+		if(depth==6) {
+			for(int i=0; i<6; i++) {
+				sb.append(ans[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=start; i<N; i++) {
+			ans[depth]= arr[i];
+			dfs(depth+1, i+1);
+		}
+		
+	}
 }
