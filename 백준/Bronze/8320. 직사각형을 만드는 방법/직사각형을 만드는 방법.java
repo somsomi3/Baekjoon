62bfc49f231c8677efb64674c22a80d033acc611
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-12 10:58:06 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; i * j <= N; j++) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
