2526b91f843ee74b329a539f6d6ef06c622f7d0f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-14 15:26:03 +0900

    [Silver V] Title: 피카츄, Time: 140 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/14405. 피카츄/피카츄.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int n = s.length();
+        int i = 0;
+
+        while (i < n) {
+            if (i + 1 < n &&
+                s.charAt(i) == 'p' &&
+                s.charAt(i + 1) == 'i') {
+
+                i += 2;
+
+            } else if (i + 1 < n &&
+                       s.charAt(i) == 'k' &&
+                       s.charAt(i + 1) == 'a') {
+
+                i += 2;
+
+            } else if (i + 2 < n &&
+                       s.charAt(i) == 'c' &&
+                       s.charAt(i + 1) == 'h' &&
+                       s.charAt(i + 2) == 'u') {
+
+                i += 3;
+
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
+}
