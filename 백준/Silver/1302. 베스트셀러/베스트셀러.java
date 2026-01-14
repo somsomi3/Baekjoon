86c46e3c950c7dbf31aa1929dc095bf3680e8c3b
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-14 20:22:23 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -2,29 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            String book = br.readLine();
-            map.put(book, map.getOrDefault(book, 0) + 1);
-        }
-
+		int N = Integer.parseInt(br.readLine());
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<N; i++) {
+			String s = br.readLine();
+			
+			map.put(s, map.getOrDefault(s, 0)+1);
+			
+		}
         int max = 0;
-        String answer = "";
+        String ans = "";
 
         for (String key : map.keySet()) {
             int cnt = map.get(key);
 
-            if (cnt > max || (cnt == max && key.compareTo(answer) < 0)) {
+            if (cnt > max || (cnt == max && key.compareTo(ans) < 0)) {
                 max = cnt;
-                answer = key;
+                ans = key;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }
