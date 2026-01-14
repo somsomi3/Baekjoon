5ca7e1a4ba4c830e8295e85adda21f5b9ef289e2
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-14 21:30:51 +0900

    [Silver I] Title: 용돈 관리, Time: 224 ms, Memory: 22580 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -2,56 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        int maxVal = 0;
-        long sum = 0;
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            maxVal = Math.max(maxVal, arr[i]);
-            sum += arr[i];
-        }
-
-        long left = maxVal; // 최소 하루치 이상
-        long right = sum;   // 전체 합
-        long answer = right;
-
-        while (left <= right) {
-            long mid = (left + right) / 2;
-
-            if (canUse(mid)) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        System.out.println(answer);
-    }
-
-    static boolean canUse(long money) {
-        int count = 1; // 첫 인출
-        long remain = money;
-
-        for (int i = 0; i < N; i++) {
-            if (remain < arr[i]) { // 오늘 쓸 돈 부족 >> 다시 인출
-                count++;
-                remain = money;
-            }
-            remain -= arr[i];
-        }
-
-        return count <= M;
-    }
+	
+	static int[] arr;
+	static int N, M;
+	static int sum=0;
+	
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		for(int i =0; i<N; i++) {
+			arr[i]= Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		int max = 0;
+		//최솟값주의 
+		for (int x : arr) max = Math.max(max, x);
+		int left = max;
+		int right = sum;
+		
+		int ans =1_000_000_001;
+		while(left<=right) {
+			int mid = (left+right)/2;
+			
+			if(canCount(mid)<=M) {
+				right= mid-1;
+				ans = Math.min(mid,ans);
+			}else left =mid+1;
+		}
+		
+		System.out.println(ans);
+	}
+	static int canCount(int mid) {
+		int count =1; 
+		int a = mid;
+		for(int i=0; i<N; i++) {
+			if(arr[i]<=a) {
+				a-= arr[i];
+			}else if(arr[i]>a) {
+				count++;
+			    a = mid - arr[i];
+			}
+		}
+		return count;
+	}
 }
