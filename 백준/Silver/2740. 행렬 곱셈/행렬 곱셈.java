6798ee1cb6a576ba880226a99d9a108ec926187c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-17 17:03:14 +0900

    [Silver V] Title: 행렬 곱셈, Time: 164 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int M2 = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] B = new int[M2][K];
+        for (int i = 0; i < M2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] C = new int[N][K];
+
+        
+        //인덱스 i j k 가 순서가 바뀌어도 상관없음. 
+        for (int i = 0; i < N; i++) {//행
+            for (int j = 0; j < K; j++) {//열
+                for (int k = 0; k < M; k++) {//곱해서 누적하는 인덱스 
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                sb.append(C[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
