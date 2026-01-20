62698bf3568c8a00deff918a2438aa9d53ab5135
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-20 20:17:41 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,33 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i =0; i<N;i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
-		}
-		 Arrays.sort(arr);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int count = 0;
-		int start = 0;
-		int end = N-1;
-		
-		while(start<end) {
-			int sum = arr[start]+arr[end];
-			if(sum==M) {
-				count++;
-				start++;
-				end--;
-            } else if(sum < M) {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = N - 1;
+        int count = 0;
+
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (sum < M) {
                 start++;
+            } else if (sum > M) {
+                end--;
             } else {
+                count++;
+                start++;
                 end--;
             }
         }
