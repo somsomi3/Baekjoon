765dd5a51015d46dda91db4d0e15af8bbd5736c7
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-20 21:49:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 124 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -9,35 +9,41 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N+1];
-		int[] prefix = new int[N+1];
+		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i =1; i<=N; i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
-			prefix[i] = prefix[i-1]+arr[i];
-			
+		for(int i =0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int left = 1;
-		int right = 1;
-//		int sum =0;
-		
-		int count =0;
-		
-//		while(left<right) {
-		while(right <= N) {
-			
-			int sum = prefix[right] - prefix[left - 1];
-			
-			if(sum<M)right++;
-//			else if(sum==M)count++;
-//			else left++;
-			else {
-				if(sum==M)count++;
-				left++;
-			}
+		int left = 0;
+//		int right = 1;
+		int right = 0;
+		int sum = arr[0];
 	
+		int count =0;
+//		while(right<N) { //=>left 이동부분이 애매해짐.
+		while (true) {	
+//			sum= arr[left]+arr[right];
+			
+			//양수 배열에선 left를 줄이는 게 정석
+			if(sum>M) {
+				sum-=arr[left];
+				left++;
+				
+			}else if(sum<M) { //맨처음 경우가 여기라서, right부터 시작해도 중복수는 없음.
+				right++;
+		        if (right == N) break;
+				sum+=arr[right];
+				
+			}else {
+				//0에서 시작해서 오른쪽으로 나아가고 잇으니, 왼쪽을 먼저 빼서 변화를 주자.
+				count++;
+		        sum -= arr[left];
+				left++;
+//				right++;
+			}
+			
 		}
-		System.out.print(count);	
+		System.out.println(count);
 	}
 }
