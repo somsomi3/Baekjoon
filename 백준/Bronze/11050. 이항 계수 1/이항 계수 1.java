267dff490d51d51219fae5e42bb74ed39e86e4cc
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-21 14:05:18 +0900

    [Bronze I] Title: 이항 계수 1, Time: 92 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,23 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int fact(int x) {
-		int one =1;
-		for(int i=1; i<=x; i++) {
-			one*=i;
-		}
-		return one;
-	}
-	
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int ans = fact(N)/ (fact(N-K)*fact(K));
-		System.out.println(ans);
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int res = 1;
+
+        for (int i = 1; i <= K; i++) {
+            res = res * (N - i + 1) / i;
+        }
+
+        System.out.println(res);
+    }
 }
