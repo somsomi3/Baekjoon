3713d35d3d96191d1d2087d008202c403c193287
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-22 09:53:52 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = "WelcomeToSMUPC";
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(s.charAt((N-1)%14));
+	}
+}
