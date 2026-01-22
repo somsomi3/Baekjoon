8f4d4069eb187b36eb2b5a5ebfc58739d1a8f434
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-22 16:40:59 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 512 ms, Memory: 142188 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,43 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static List<Integer>[] adj;
-    static boolean[] visited;
+	static List<Integer>[] graph;
+	static int[] dist;
+	static boolean[] visited;
+	static int N, M;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken()); 
+		
+		//graph와 dist선언
+		graph = new ArrayList[N+1];
+		for(int i =1; i<=N; i++)graph[i] = new ArrayList<>();
+		dist = new int[N+1];
+		visited = new boolean[N+1];
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			//무방향= 양방향
+			graph[a].add(b);
+			graph[b].add(a);
+			
+		}
+		int count = 0;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        adj = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adj[a].add(b);
-            adj[b].add(a);
-        }
-
-        int cnt = 0;
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                cnt++;
-            }
-        }
-
-        System.out.println(cnt);
-    }
-
-    static void dfs(int cur) {
-        visited[cur] = true;
-        for (int next : adj[cur]) {
-            if (!visited[next]) dfs(next);
-        }
-    }
+		for (int i = 1; i <= N; i++) {
+		    if (!visited[i]) {
+		        dfs(i);
+		        count++;
+		    }
+		}
+		System.out.println(count);
+		
+	}
+	
+	static void dfs(int cur) {
+		visited[cur] = true;
+	
+		for(int next: graph[cur]) {
+			if(!visited[next])		
+				dfs(next);
+		}
+	}
 }
