d6ca508e2c76410bcb186db7e09f5f577ab67318
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-25 17:35:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int count =0;
-		for(int i =0; i<N; i++) {
-			String r = br.readLine();
+		
+		int ans= 0;
+		while(N-->0) {
+			String s = br.readLine();
 			
-			//시작점 하나 잡고, 그것과 다음c 비교하기..
+			int[] count = new int[26];
+			boolean flag = true;
 			
-			boolean[] arr = new boolean[26];
-			//굳이 안채워도 기본값이 false?
-			Arrays.fill(arr, false);
+			count[s.charAt(0)-'a']++;
 			
-//			int prev =0;
-			//문자열 끼리 비교할 것
-			char prev =0;
-			boolean ok = true; // 그룹 단어 여부
-
-			for(int j = 0; j<r.length(); j++) {
-				char c = r.charAt(j);
-				if(c != prev) {
-					if(arr[c-'a']) {
-						ok = false;
-						break;
-					}else {
-						arr[c-'a'] = true;
-
-					}
-					
-				}prev =c;
-			}if(ok) count++; 
-		}System.out.println(count);
+			for(int i=1; i<s.length(); i++) {
+				if(s.charAt(i-1)!=s.charAt(i)&&count[s.charAt(i)-'a']>0) {
+					flag = false;
+					break;
+				}else{
+					count[s.charAt(i)-'a']++;
+				}
+			}
+			if(flag)
+				ans++;	
+		}
+		System.out.println(ans);
 	}
-
 }
