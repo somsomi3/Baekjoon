7464e67b92bcbc9604ceb76e42930283ef6c2134
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-01-27 10:11:11 +0900

    [Bronze III] Title: 완전 제곱수, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt =0;
+		for(int i=1; i<=500; i++) {
+			
+			int x = i*i+N;
+			int a = (int)Math.sqrt(x);
+			
+			if(a<=500 &&a>=i &&a*a==x) {
+				//A>B:  && a > i
+				//정수 문제에서는 반드시 역검증해야해서: &&a*a==x
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
