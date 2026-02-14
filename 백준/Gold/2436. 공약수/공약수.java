85c66cd47b96908e946e7b40044c84a8f47e26fb
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-14 21:39:18 +0900

    [Gold V] Title: 공약수, Time: 116 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.java
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -2,39 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        long x = M / N;
+
+        long a = 1, b = x;
+
+        for(long i = 1; i * i <= x; i++) {
+            if(x % i == 0) {
+                long j = x / i;
+
+                if(gcd(i, j) == 1) {
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+
+        System.out.println(N * a + " " + N * b);
+    }
 
     static long gcd(long a, long b) {
-        while (b != 0) {
+        while(b != 0) {
             long r = a % b;
             a = b;
             b = r;
         }
         return a;
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        long G = Long.parseLong(st.nextToken());
-        long L = Long.parseLong(st.nextToken());
-
-        long x = L / G;
-
-        long p = 0, q = 0;
-
-        for (long i = 1; i * i <= x; i++) {
-            if (x % i == 0) {
-                long a = i;
-                long b = x / i;
-
-                if (gcd(a, b) == 1) {
-                    p = a;
-                    q = b;
-                }
-            }
-        }
-
-        System.out.println((p * G) + " " + (q * G));
     }
 }
