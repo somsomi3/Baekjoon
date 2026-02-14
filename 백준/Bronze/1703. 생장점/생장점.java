3ee9a8c07e9c896be266e319fa432896a6eaf3d5
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-14 16:32:23 +0900

    [Bronze III] Title: 생장점, Time: 100 ms, Memory: 14004 KB -BaekjoonHub

--- 백준/Bronze/1703. 생장점/생장점.java
+++ 백준/Bronze/1703. 생장점/생장점.java
@@ -12,16 +12,19 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(x);
 			
 			int N = Integer.parseInt(st.nextToken());
-		
-			int z = 1;
-
-			for(int i =0; i<N; i++) {
-				int branch = Integer.parseInt(st.nextToken());
-				int minuses = Integer.parseInt(st.nextToken());
-				
-				z =z*branch -minuses;
+			
+			int stand = Integer.parseInt(st.nextToken());
+			int minus = Integer.parseInt(st.nextToken());
+			
+			int z = stand- minus;
+			if(N>1) {
+				for(int i =1; i<N; i++) {
+					int branch = Integer.parseInt(st.nextToken());
+					int minuses = Integer.parseInt(st.nextToken());
+					
+					z =z*branch -minuses;
+				}
 			}
-		
 			System.out.println(z);
 			
 		}
