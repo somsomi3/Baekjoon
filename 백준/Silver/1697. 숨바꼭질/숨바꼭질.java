c5cba5f24d34add1391317d8d37161d6b669979c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-15 16:46:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 20804 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,33 +4,50 @@ import java.util.*;
 public class Main {
     static final int MAX = 100001;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());  // 수빈이 위치
-        int K = Integer.parseInt(st.nextToken());  // 동생 위치
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] time = new int[MAX]; // 방문 여부 및 걸린 시간 저장용
+        if (N == K) {
+            System.out.println(0);
+            return;
+        }
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(N);  // 시작점
-        time[N] = 1;     // 0초가 아닌 1로 초기화해서 나중에 -1 해서 출력
+        //정점 자체의 visited, dist
+        boolean[] visited = new boolean[MAX];
+        
+        //dist[x] : N에서 x까지 걸린 최소 시간
+        int[] dist = new int[MAX];
 
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
+        //BFS
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(N);
+        visited[N] = true;
 
-            if (now == K) {
-                System.out.println(time[now] - 1);  // 실제 걸린 시간은 -1
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (cur == K) {
+                System.out.println(dist[cur]);
                 return;
             }
 
-            for (int next : new int[]{now - 1, now + 1, now * 2}) {
-                if (next >= 0 && next < MAX && time[next] == 0) {
-                    queue.offer(next);
-                    time[next] = time[now] + 1;
+            //현재 위치에서 갈 수 있는 3가지 이동 정의
+            //방향벡터 같은 역할
+            // 최단거리 보장
+            int[] next = {cur - 1, cur + 1, cur * 2};
+
+            for (int nx : next) {
+                if (nx >= 0 && nx < MAX && !visited[nx]) {//범위 안에 있고, 방문하지 않았다면
+                    visited[nx] = true;
+                    dist[nx] = dist[cur] + 1;
+                    q.offer(nx);
                 }
             }
         }
     }
+
 }
