8868eda88a2e8eae65214d3d33dcbfe08cbc1a0f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-15 20:18:17 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 872 ms, Memory: 237948 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,56 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<Integer>[] graph;
-    static int[] dist;
-    static int N, M, K, X;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
-        dist = new int[N + 1];
+	static int N, M, K, X;
+	
+	static List<Integer>[] graph;
+	static int[] dist;
+	static boolean[] visited;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList[N+1];
+		for(int i = 1; i<=N; i++)graph[i]= new ArrayList<>();
+		
+		visited = new boolean[N+1];
+		dist = new int[N+1];
+//		Arrays.fill(dist, Integer.MAX_VALUE);
         Arrays.fill(dist, -1);
+		
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(b);
+		}
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-        }
-
-        bfs(X);
-
-        boolean found = false;
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] == K) {
-                System.out.println(i);
-                found = true;
-            }
-        }
-
-        if (!found) System.out.println(-1);
-    }
-
-    static void bfs(int start) {
-    	//처음 도착한 순간이 무조건 최단 거리
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        dist[start] = 0;
-
-        while (!q.isEmpty()) {
+		dijkstra(X);
+		
+		boolean flag = false;
+		for(int i = 1; i <= N; i++) {
+			if(dist[i]==K) {
+				System.out.println(i);
+				flag = true;
+			}
+		}
+		if(!flag)System.out.println(-1);
+	}
+	
+	static void dijkstra(int start) {
+//		PriorityQueue<Integer>[] pq = new PriorityQueue<>();
+		Queue<Integer> q = new LinkedList<>();
+		dist[start] = 0;
+		q.add(start);
+		
+        while(!q.isEmpty()) {
             int cur = q.poll();
 
-            for (int next : graph[cur]) {
-                if (dist[next] == -1) {
+            for(int next : graph[cur]) {
+                if(dist[next] == -1) {
                     dist[next] = dist[cur] + 1;
                     q.add(next);
                 }
