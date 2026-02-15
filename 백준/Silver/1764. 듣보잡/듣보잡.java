647824204607ad53ba048909954411f7a821b2ab
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-15 14:12:46 +0900

    [Silver IV] Title: 듣보잡, Time: 228 ms, Memory: 23864 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,42 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		HashSet<String> set = new HashSet<>();
-		List<String> ans = new ArrayList<>();
-		
-		for(int i = 0; i<N; i++) {
-			String s = br.readLine();
-			set.add(s);
-		}
-		
-		int cnt = 0;
-		
-		for(int i = 0; i<M; i++) {
-			String t = br.readLine();
-			
-			if(set.contains(t)) {
-				cnt++;
-				ans.add(t);
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		
-		System.out.println(cnt);
-		
-		//사전 순 출력,...
-		Collections.sort(ans);  // 사전순 정렬
-		
-		for(String s : ans) {
+    static boolean binarySearch(String[] arr, String target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int cmp = arr[mid].compareTo(target);
+
+            if (cmp == 0) return true;
+            else if (cmp < 0) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+
+        List<String> ans = new ArrayList<>();
+
+        for(int i = 0; i < M; i++) {
+            String target = br.readLine();
+
+            if(binarySearch(arr, target)) {
+                ans.add(target);
+            }
+        }
+
+        Collections.sort(ans);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(ans.size()).append("\n");
+
+        for(String s : ans) {
             sb.append(s).append("\n");
         }
 
-		System.out.println(sb);
-	}
+        System.out.print(sb);
+    }
 }
