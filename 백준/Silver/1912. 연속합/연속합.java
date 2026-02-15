800760276f17017b7964391be9f2356142953590
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-15 11:59:53 +0900

    [Silver II] Title: 연속합, Time: 196 ms, Memory: 23248 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,21 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		
-		int sum = 0;
-		int max = -1001;
-		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<N; i++) {
-			int x = Integer.parseInt(st.nextToken());
-			sum = Math.max(x, sum + x);
-		    max = Math.max(max, sum);
-		}
-		
-		System.out.println(max);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = Integer.parseInt(st.nextToken());
+        int max = sum;
+
+        for (int i = 1; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+
+            sum = Math.max(x, sum + x);
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
 }
