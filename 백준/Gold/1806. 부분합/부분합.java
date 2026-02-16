d4142ef8222b5cea803e1ec64f003e9ddc2f1010
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-16 23:26:49 +0900

    [Gold IV] Title: 부분합, Time: 244 ms, Memory: 24028 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -6,32 +6,41 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N= Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[N];
-		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
+		for(int i =0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		int sum = 0;
 		
-        int max = sum;
-        int start =0;
-        int min=100001;
-        for(int i=0; i<N; i++) {
-//            if(sum<K)sum += arr[i];
-        	sum += arr[i];
-            while(sum >= K) {
-            	if(min>(i-start+1))min=(i-start+1);
-            	sum -= arr[start];
-            	start++;
-            }
-
+		int left = 0;
+		int right = 0;
+        int min = Integer.MAX_VALUE;
+		int sum =0;
+		//만약 맨처음 값이.. S보다 크다면...
+		//sum이 0이라서 맨처음에는 항상 확장 부분으로 가. 그래서 left랑 right가 뒤집힐 위험은 없음.
+		boolean flag = false;
+//		while(right<N) {
+		//right가 N이 되면 left를 더 줄여서 길이 갱신이 가능하다.
+		while (true) {
+		    if (sum >= S) {
+		    	flag = true;
+				min= Math.min(min, right - left);
+				sum -= arr[left];
+				left++;	
+			}
+		    else if (right == N) {
+		        break;
+		    } 
+			else {
+				sum+=arr[right]; 
+				right++;
+			}
 		}
-//		System.out.println(min);
-        System.out.println(min == 100001 ? 0 : min);
+		if(!flag)System.out.println(0);
+		else System.out.println(min);
+	
 	}
 }
