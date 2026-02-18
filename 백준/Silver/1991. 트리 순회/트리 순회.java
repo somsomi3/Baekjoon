d9b2ef9721912d246573a0ca943678c246131535
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-18 15:31:28 +0900

    [Silver I] Title: 트리 순회, Time: 96 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,49 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static char[][] tree;
+//	static List<String>[] graph;
+	static char[][] tree;
+	static boolean[] visited;
+	static int N;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		tree = new char[N+1][2];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        tree = new char[26][2];
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-
-            tree[root - 'A'][0] = left;
-            tree[root - 'A'][1] = right;
-        }
-
-        preorder('A');
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char root = st.nextToken().charAt(0);
+			char left = st.nextToken().charAt(0);
+			char right = st.nextToken().charAt(0);
+			
+			tree[root - 'A'][0] = left;
+			tree[root - 'A'][1] = right;
+			
+		}
+//		전위
+        pre('A');
         System.out.println();
-        inorder('A');
+        in('A');
         System.out.println();
-        postorder('A');
-    }
-
-    static void preorder(char node) {
-        if (node == '.') return;
-        System.out.print(node);
-        preorder(tree[node - 'A'][0]);
-        preorder(tree[node - 'A'][1]);
-    }
-
-    static void inorder(char node) {
-        if (node == '.') return;
-        inorder(tree[node - 'A'][0]);
-        System.out.print(node);
-        inorder(tree[node - 'A'][1]);
-    }
-
-    static void postorder(char node) {
-        if (node == '.') return;
-        postorder(tree[node - 'A'][0]);
-        postorder(tree[node - 'A'][1]);
-        System.out.print(node);
-    }
+        post('A');
+		
+		
+	}
+	static void pre(char node) {
+		if(node =='.')return;
+		
+		System.out.print(node);
+		pre(tree[node - 'A'][0]);
+		pre(tree[node - 'A'][1]);
+	}
+	static void in(char node) {
+		if(node =='.')return;
+		
+		in(tree[node - 'A'][0]);
+		System.out.print(node);
+		in(tree[node - 'A'][1]);
+	}
+	
+	static void post(char node) {
+		if(node =='.')return;
+		
+		post(tree[node - 'A'][0]);
+		post(tree[node - 'A'][1]);
+		System.out.print(node);
+		
+	}
+	
 }
