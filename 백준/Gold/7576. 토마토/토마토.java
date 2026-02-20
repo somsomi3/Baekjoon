d45dbd6be28714cc7183f175db79eb5e1fe08782
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-20 22:05:25 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 104304 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,88 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[][] arr;
-	static int[][] dist;
+	static int[][] graph;
 	static boolean[][] visited;
-//	static final int[] dx = {0, 0, 0, 0};
-//	static final int[] dy = {-1, 1, 0, 0};
-//	static final int[] dz = {0, 0, 1, -1};
-	static final int[] dx = {1, -1, 0, 0};
-	static final int[] dy = {0, 0, -1, 1};
+	static int N, M;
+//	static int[][] dist;
+	static int[] dx = {0, 0, -1, 1};
+	static int[] dy = {-1, 1, 0, 0};
+	static Queue<int[]> q = new ArrayDeque<>();
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		//6, 4는 M, N
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N][M];// 세로N 가로M
-        visited = new boolean[N][M];
-        dist = new int[N][M];
-        
-        Queue<int[]> q = new ArrayDeque<>();
-		for(int i =0; i<N; i++) {
+		graph = new int[N][M];
+		visited = new boolean[N][M];
+
+		for(int i =0 ; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j]==1)q.offer(new int[] {i,j});
-			}
-		}
-//		for(int i =0; i<N; i++) {
-//			st = new StringTokenizer(br.readLine());
-//			for(int j=0; j<M; j++) {
-//				Queue<int[]> q = new ArrayDeque<>();
-//				if(arr[i][j] == 1) {
-//					q.offer(new int[] {i, j});
-//				}
-//			}
-//		}
-		
-		
-		bfs(q);
-		
-		int ans = 0;
-		for(int i= 0; i<N; i++) {
-			for(int j =0; j<M; j++) {
-//				if(arr[i][j] =0 || arr[i][j] =1) {
-				if(arr[i][j] == 0) {
-					System.out.println("-1");
-					return;
+			for(int j =0 ; j<M; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+				if (graph[i][j] == 1) {
+		            q.offer(new int[]{i, j}); 
 				}
-               ans = Math.max(ans, dist[i][j]);
 			}
 		}
-		System.out.println(ans);
-	}
-	
-	static void bfs(Queue<int[]> q) {
-//		Queue<int[]> qq = new ArrayDeque<>();
-//		visited[x][y]= true;
-//		dist[x][y] = 1;
-//        qq.offer(new int[]{x, y});
-		
-		while(!q.isEmpty()) {
+		bfs();
+        
+		int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (graph[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                max = Math.max(max, graph[i][j]);
+            }
+        }
+        //토마토는 원래 초기에 1이어서, -1 을 해서 진짜 최소날짜를 구해주는 것.
+        System.out.println(max - 1);
+    }
+    static void bfs() {
+        while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-			
-			for(int k =0; k<4; k++) {
-                int nx = cx + dx[k];
-                int ny = cy + dy[k];
-				
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-//                if (visited[nx][ny] || arr[nx][ny] == 0) continue;
-                if (arr[nx][ny] != 0) continue;
-			
-                arr[nx][ny] = 1; // 익은 표시 추가 
-				visited[nx][ny]= true;
-                dist[nx][ny] = dist[cx][cy] + 1;
+                if (graph[nx][ny] != 0) continue;
+
+                graph[nx][ny] = graph[x][y] + 1;
                 q.offer(new int[]{nx, ny});
-				
-			}
-		}
-	}
+            }
+        }
+    }
 }
