cd2d15ba33c32f51d7388a46a7048ca7268c05d3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-21 22:35:23 +0900

    [Bronze II] Title: 단어의 개수, Time: 256 ms, Memory: 51424 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -4,18 +4,16 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		String s = br.readLine().trim();
-
-		if (s.isEmpty()) {
+        if (s.isEmpty()) {
 		    System.out.println(0);
 		    return;
 		}
-
-		String[] ss = s.split(" ");
+		String[] n = s.split(" ");
 		
-		int x = ss.length;
-
-		System.out.println(x);
+		System.out.println(n.length);
+//		System.out.println(s);
 		
 	}
 }
