a4e4e92efbc0137358ac7a517ac2c60efc4e125f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-02-23 07:57:49 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		
+		long sum = A+B+C;
+		
+		System.out.println(sum);
+		
+	}
+}
