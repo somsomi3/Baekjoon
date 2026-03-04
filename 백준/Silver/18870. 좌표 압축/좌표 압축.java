22431c70596400c56fb9aa89888fd90000f3b1e9
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-04 21:26:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1640 ms, Memory: 180980 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,35 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        int[] original = new int[N];
-        int[] sorted = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            original[i] = num;
-            sorted[i] = num;
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(sorted);
+        int[] copy = arr.clone();
+        Arrays.sort(copy);
 
-        // 중복 제거하면서 값 → 인덱스 매핑
-        Map<Integer, Integer> map = new HashMap<>();
-        int idx = 0;
-        for (int i = 0; i < N; i++) {
-            if (!map.containsKey(sorted[i])) {
-                map.put(sorted[i], idx++);
+        //크기변경이 가능하게 하기위해서 list 사용
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(copy[0]);
+
+        for(int i = 1; i < N; i++){
+            if(copy[i] != copy[i-1]){
+                list.add(copy[i]);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(map.get(original[i])).append(' ');
+        int[] unique = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            unique[i] = list.get(i);
         }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            int left = 0;
+            int right = unique.length - 1;
+            while(left <= right){
+
+                int mid = left + (right - left) / 2;
+                if(unique[mid] >= arr[i]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+
+            sb.append(left).append(" ");
+        }
+
         System.out.println(sb);
     }
+
 }
