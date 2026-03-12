0886a19275e6a3f6fa6ceff90c74806139e6f202
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-12 14:53:46 +0900

    [Silver III] Title: N과 M (1), Time: 248 ms, Memory: 22440 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,39 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static boolean[] used;
-    static StringBuilder sb = new StringBuilder();
-
-    static void dfs(int depth) {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) sb.append(arr[i]).append(' ');
-            sb.append('\n');
-            return;
-        }
-
-        for (int i = 1; i <= N; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                arr[depth] = i;
-                dfs(depth + 1);
-                used[i] = false;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[M];
-        used = new boolean[N + 1];
-
-        dfs(0);
+	static int N, M;
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N+1];
+		visited = new boolean[N+1];
+		dfs(1, 0);//시작과 depth시작
+		
         System.out.print(sb);
-    }
+	}
+	static void dfs(int start, int depth) {
+		
+		if(depth==M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 1; i<= N; i++) {
+			if(!visited[i]) {
+				arr[depth] =i;
+				visited[i] = true;
+				dfs(start+1, depth+1);
+				visited[i] = false;
+			}
+
+		}	
+	}
 }
