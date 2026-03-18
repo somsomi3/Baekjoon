dbc80afaa4baf683e0bc48625dbb312e3ac3db3b
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-18 20:30:08 +0900

    [Silver III] Title: N번째 큰 수, Time: 896 ms, Memory: 244068 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -2,24 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int a = Integer.parseInt(st.nextToken());
-                pq.offer(a);
-                if (pq.size() > N) {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		//최소힙
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				pq.offer(num);
+                
+                if(pq.size() > N) {
+                	//가장 작은 수가 빠져나옴.
                     pq.poll();
                 }
+                //PQ의 상태
+                //[35, 41, 48, 49, 52]
             }
         }
-
         System.out.println(pq.peek());
-    }
+    }	
 }
+
