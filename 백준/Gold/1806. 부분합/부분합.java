f686e60117baf66081fda89aac3aecacdada6c02
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-19 00:38:12 +0900

    [Gold IV] Title: 부분합, Time: 244 ms, Memory: 23912 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -11,36 +11,25 @@ public class Main {
 		
 		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<N; i++) {
+		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int left = 0;
-		int right = 0;
-        int min = Integer.MAX_VALUE;
-		int sum =0;
-		//만약 맨처음 값이.. S보다 크다면...
-		//sum이 0이라서 맨처음에는 항상 확장 부분으로 가. 그래서 left랑 right가 뒤집힐 위험은 없음.
-		boolean flag = false;
-//		while(right<N) {
-		//right가 N이 되면 left를 더 줄여서 길이 갱신이 가능하다.
-		while (true) {
-		    if (sum >= S) {
-		    	flag = true;
-				min= Math.min(min, right - left);
-				sum -= arr[left];
-				left++;	
-			}
-		    else if (right == N) {
-		        break;
-		    } 
-			else {
-				sum+=arr[right]; 
-				right++;
-			}
-		}
-		if(!flag)System.out.println(0);
-		else System.out.println(min);
-	
+		int l = 0;
+		int r = 0;
+		int sum = 0;
+		int min = N+1;
+		while(true) {
+            if(sum >= S) {
+                min = Math.min(min, r - l);
+                sum -= arr[l++];
+            } else if(r == N) {
+                break;
+            } else {
+                sum += arr[r++];
+            }    
+        }
+		if(min == N+1) System.out.println(0);
+        else System.out.println(min);
 	}
 }
