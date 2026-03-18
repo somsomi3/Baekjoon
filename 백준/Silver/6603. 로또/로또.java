54e4b0be5a4dd1582b7e14d8b7a8818b72f0b410
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-18 22:16:56 +0900

    [Silver II] Title: 로또, Time: 292 ms, Memory: 20296 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -2,43 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] arr;
-	static int[] ans = new int[6];
 	static int N;
-    static StringBuilder sb = new StringBuilder();
-    
+	static int[] arr;
+	static int[] path;
+
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		while(true) {
-			String line = br.readLine();
-			if(line.equals("0"))break;
+			String s = br.readLine();
+			if(s.equals("0"))break;
+			StringTokenizer st = new StringTokenizer(s);
 			
-			StringTokenizer st = new StringTokenizer(line);
 			N = Integer.parseInt(st.nextToken());
 			
+			path = new int[N];
 			arr = new int[N];
-			for(int i =0; i<N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+			for(int i = 0; i<N; i++) {
+				arr[i]= Integer.parseInt(st.nextToken());
 			}
-			dfs(0,0);
-			sb.append("\n");
-		}
-        System.out.print(sb);
-	}
-	static void dfs(int depth, int start) {
-		if(depth==6) {
-			for(int i=0; i<6; i++) {
-				sb.append(ans[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i=start; i<N; i++) {
-			ans[depth]= arr[i];
-			dfs(depth+1, i+1);
+			dfs(0, 0);
+			System.out.println();
 		}
 		
+		
+	}
+	static void dfs(int start, int depth) {
+		if(depth==6) {
+			for(int i = 0; i < 6; i++) {
+	            System.out.print(path[i] + " ");
+	        }
+	        System.out.println();
+	        return;
+	    }
+		
+		for(int i = start; i<N ; i++) {
+			path[depth] = arr[i];  
+			dfs(i+1, depth+1);
+		}
 	}
 }
