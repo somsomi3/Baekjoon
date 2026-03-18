d65e6023b6a75f7e96b3c7ef76031c38741fa1d6
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-18 17:27:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 576 ms, Memory: 46444 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,36 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Person{
-		int age;
-		String name;
-		Person(int age, String name){
-			this.age = age;
-			this.name = name;
-		}
-	}
-	
+    static class Edge {
+        int age;
+        String name;
+
+        Edge(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-        List<Person> list = new ArrayList<>();
-		for(int i =0; i<N; i++) {
+		List<Edge> list = new ArrayList<>();
+		
+		//맵은 인덱스가 없어서 ArrayList로 저장해보자!
+		for(int i = 0; i<T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            String s = st.nextToken();
-            
-            list.add(new Person(x, s));
-        }
-		
-		list.sort((a, b)-> a.age - b.age);
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+			
+            list.add(new Edge(age, name));
+		}
+		//어차피 나이만 정렬하면, 위의 입력에서 인덱스 순으로 들어갔기 때문에 2차적으로 인덱스대로 정렬된다.
+		Collections.sort(list, (a, b)-> a.age - b.age);
 		
 		StringBuilder sb = new StringBuilder();
-        for (Person p : list) {
-            sb.append(p.age).append(" ").append(p.name).append("\n");
-        }
-
-        System.out.print(sb);
-    }
+		
+		for(Edge x : list) {
+			sb.append(x.age).append(" ").append(x.name).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
 }
