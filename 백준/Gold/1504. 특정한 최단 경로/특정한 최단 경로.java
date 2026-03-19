1beae8ce494ff10b78fe090ed9c30f6e11e5d2cc
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-19 19:57:52 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 524 ms, Memory: 68628 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -10,12 +10,10 @@ public class Main {
 			this.cost = cost;
 		}
 	}
-	
-	//점을 주로 한다. 고로 List<>[] graph
-	static List<Edge>[] graph;
 	static int N, E;
-    static int[] dist;
-    static final int INF = 200000000;
+	static List<Edge>[] graph;
+	static int[] dist;
+    static final int INF = Integer.MAX_VALUE;
     
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,79 +22,73 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
-		graph = new ArrayList[N+1];
-		for(int i = 1; i <= N; i++)graph[i] = new ArrayList<>();
+		graph= new ArrayList[N+1];
+		for(int i = 1; i<=N; i++)graph[i] = new ArrayList<>();
 		dist = new int[N+1];
-	    
-		for(int i = 0; i< E; i++) {
+		Arrays.fill(dist, -1);
+		
+		for(int i = 0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 			
-			//무방향 = 양방향
-			graph[from].add(new Edge(to, cost));
-			graph[to].add(new Edge(from, cost));
+			graph[a].add(new Edge(b, c));
+			graph[b].add(new Edge(a, c));
 			
 		}
 		
-		st= new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		int v1 = Integer.parseInt(st.nextToken());
 		int v2 = Integer.parseInt(st.nextToken());
 		
-//		//고정값 다익스트라
-//		dijkstra(v1, v2);
-//		//시작에서 끝 
-//		dijkstra(1, v1);
-//		dijkstra(v2, N);
-//		dijkstra(1, v2);
-//		dijkstra(v1, N);
-		
-        
-//		1 → v1 → v2 → N
-//		1 → v2 → v1 → N
-		//1번 정점에서 모든 정점까지의 최단거리
+		//dijkstra(시작점)
+		//1 → v1 → v2 → N
+		//1 → v2 → v1 → N
 		dijkstra(1);
-        int d1v1 = dist[v1];
-        int d1v2 = dist[v2];
+		//dist[v1], dist[v2]
+		int oneToV1 = dist[v1];
+        int oneToV2 = dist[v2];
 
-        dijkstra(v1);
-        int dv1v2 = dist[v2];
-        int dv1N = dist[N];
-
-        dijkstra(v2);
-        int dv2v1 = dist[v1];
-        int dv2N = dist[N];
+		dijkstra(v1);
+		//dist[v2], dist[N]
+		int v1ToV2 = dist[v2];
+        int v1ToN = dist[N];
         
+		dijkstra(v2);
+		//dist[v1], dist[N]
+        int v2ToV1 = dist[v1];
+        int v2ToN = dist[N];
         
-        long case1 = (long)d1v1 + dv1v2 + dv2N;
-        long case2 = (long)d1v2 + dv2v1 + dv1N;
+        long case1 = (long) oneToV1 + v1ToV2 + v2ToN;
+        long case2 = (long) oneToV2 + v2ToV1 + v1ToN;
 
-        if (d1v1 >= INF || dv1v2 >= INF || dv2N >= INF) case1 = INF;
-        if (d1v2 >= INF || dv2v1 >= INF || dv1N >= INF) case2 = INF;
+        if (oneToV1 == INF || v1ToV2 == INF || v2ToN == INF) case1 = INF;
+        if (oneToV2 == INF || v2ToV1 == INF || v1ToN == INF) case2 = INF;
 
-        long answer = Math.min(case1, case2);
+        long ans = Math.min(case1, case2);
 
-        System.out.println(answer >= INF ? -1 : answer);
+        System.out.println(ans >= INF ? -1 : ans);
     }
-	
-    static void dijkstra(int start) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-        Arrays.fill(dist, INF);
-        dist[start] = 0;
-        pq.offer(new Edge(start, 0));
-
-        while (!pq.isEmpty()) {
-        	Edge cur = pq.poll();
-
-            if (cur.cost > dist[cur.to]) continue;
-
-            for (Edge next : graph[cur.to]) {
-                if (dist[next.to] > cur.cost + next.cost) {
-                    dist[next.to] = cur.cost + next.cost;
-                    pq.offer(new Edge(next.to, dist[next.to]));
-                }
-            }
-        }
-    }
+	static void dijkstra(int start) {
+		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+	    Arrays.fill(dist, Integer.MAX_VALUE);
+	    dist[start] = 0;
+	    pq.offer(new Edge(start, 0));
+	    
+	    while (!pq.isEmpty()) {
+	        Edge cur = pq.poll();
+	        
+	        if (dist[cur.to] < cur.cost) continue;
+	        
+	        for (Edge next : graph[cur.to]) {
+	            int nextCost = dist[cur.to] + next.cost;
+	            
+	            if (dist[next.to] > nextCost) {
+	                dist[next.to] = nextCost;
+	                pq.offer(new Edge(next.to, nextCost));
+	            }
+	        }
+	    }
+	}
 }
