f0ee05279c992cb5c2a49ffe8cf55e06ad7d5bf0
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-24 16:41:49 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,35 +1,32 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int start = N - 9 * String.valueOf(N).length();
-		if (start < 0) start = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //N은 자릿수의 합
+        int N = Integer.parseInt(br.readLine());
 
-		int ans = 0;
+        int start = Math.max(1, N - 9 * String.valueOf(N).length());
 
-		for (int i = start; i < N; i++) {
-		    int temp = i;
-		    int sum = temp;
+        for (int i = start; i < N; i++) {
+            if (littleSum(i) == N) {
+                System.out.println(i);
+                return;
+            }
+        }
 
-		    while (temp > 0) {
-//		    	temp = temp / 10 → 18 / 10 = 1
-		        sum += temp % 10;
-		        temp /= 10;
-		        
-//		        sum += temp % 10 → 206 + 1 = 207
-//		        		temp = temp / 10 → 1 / 10 = 0
-		    }
+        System.out.println(0);
+    }
 
-		    if (sum == N) {
-		        ans = i;
-		        break;
-		    }
-		}
+    static int littleSum(int num) {
+        int sum = num;
 
-		System.out.println(ans);
-	}
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
 }
