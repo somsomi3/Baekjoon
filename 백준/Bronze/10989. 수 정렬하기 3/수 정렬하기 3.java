8f934d8c61fbce9a39eaf4fda0924e9dae143871
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-24 17:43:00 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2348 ms, Memory: 364096 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,22 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        int[] count = new int[10001];
-
-        for (int i = 0; i < N; i++) {
-            count[Integer.parseInt(br.readLine())]++;
-        }
-
-        for (int i = 1; i <= 10000; i++) {
-            while (count[i]-- > 0) {
-                sb.append(i).append("\n");
-            }
-        }
-        System.out.print(sb);
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr= new int[N];
+		
+		for(int i  = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());	
+		}
+		Arrays.sort(arr);
+		
+		for(int i = 0; i<N; i++) {
+			sb.append(arr[i]).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
 }
