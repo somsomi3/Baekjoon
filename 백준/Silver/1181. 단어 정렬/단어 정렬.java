8576cc303f5ca04f9805ba55a70ea6121313c93e
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-24 20:29:57 +0900

    [Silver V] Title: 단어 정렬, Time: 300 ms, Memory: 26564 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,23 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < N; i++) set.add(br.readLine());
-
-        List<String> list = new ArrayList<>(set);
-
-        Collections.sort(list, (a, b) -> {
-            if (a.length() != b.length()) return a.length() - b.length();
-            return a.compareTo(b);
-        });
-
-        StringBuilder sb = new StringBuilder();
-        for (String s : list) sb.append(s).append('\n');
-        System.out.print(sb);
-    }
-}
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		String[] arr = new String[N];
+		
+		for(int i = 0; i< N; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		Arrays.sort(arr, (a, b)->{
+			if(a.length()!= b.length()) {
+				return a.length()- b.length();
+			}
+			return a.compareTo(b);
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		//단, 중복된 단어는 하나만 남기고 제거해야 한다.
+		sb.append(arr[0]).append('\n');
+		
+		for(int i = 1; i < N; i++) {
+			if(!arr[i].equals(arr[i-1])) {
+				sb.append(arr[i]).append('\n');
+			}
+		}
+		System.out.println(sb);
+		
+	}
+}	
