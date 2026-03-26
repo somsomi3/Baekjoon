9f0842d7a65abe8afda202b977d095e3e5a1107d
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-26 16:58:32 +0900

    [Silver V] Title: 숫자 카드, Time: 1104 ms, Memory: 108320 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,30 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> cardSet = new HashSet<>();
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            cardSet.add(Integer.parseInt(st.nextToken()));
-        }
-        
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (cardSet.contains(num)) {
-                sb.append(1).append(" ");
-            } else {
-                sb.append(0).append(" ");
-            }
-        }
-        
-        System.out.println(sb.toString());
-    }
+	static int N, M;
+	static int[] arr;
+	static int[] brr;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i= 0; i<N; i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		M = Integer.parseInt(br.readLine());
+		brr = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i =0; i<M; i++) {
+			brr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i<M; i++) {
+			
+			//인덱스 값이다.
+			int left = 0;
+			
+			int right = N-1;
+			boolean flag = false;
+			
+			while(left<= right) {
+				int mid = (left+right)/2;
+				if(arr[mid]==brr[i]) {
+					flag = true;
+					break;
+				}else if( arr[mid]<brr[i]) {
+					left = mid +1;
+				}else {
+					right = mid- 1;
+				}
+			}
+			if(flag)sb.append(1).append(" ");
+			else sb.append(0).append(" ");
+		}
+		System.out.println(sb);
+		
+	}
 }
