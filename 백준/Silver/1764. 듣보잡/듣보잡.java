3248f738c9bf24b814bdcb4cd5da75601f8ad7db
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-26 17:22:17 +0900

    [Silver IV] Title: 듣보잡, Time: 252 ms, Memory: 23560 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,57 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean binarySearch(String[] arr, String target) {
-        int left = 0;
-        int right = arr.length - 1;
+    static String[] Ns;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            int cmp = arr[mid].compareTo(target);
-
-            if (cmp == 0) return true;
-            else if (cmp < 0) left = mid + 1;
-            else right = mid - 1;
-        }
-
-        return false;
-    }
-
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        String[] arr = new String[N];
+        Ns = new String[N];
 
-        for(int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+        for (int i = 0; i < N; i++) {
+            Ns[i] = br.readLine();
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(Ns); // 문자열 정렬
 
-        List<String> ans = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             String target = br.readLine();
 
-            if(binarySearch(arr, target)) {
-                ans.add(target);
+            if (binarySearch(target)) {
+                result.add(target);
             }
         }
 
-        Collections.sort(ans);
+        Collections.sort(result);
 
         StringBuilder sb = new StringBuilder();
-        sb.append(ans.size()).append("\n");
-
-        for(String s : ans) {
+        sb.append(result.size()).append("\n");
+        for (String s : result) {
             sb.append(s).append("\n");
         }
 
         System.out.print(sb);
+    }
+
+    static boolean binarySearch(String target) {
+        int left = 0;
+        int right = Ns.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int cmp = Ns[mid].compareTo(target);
+
+            if (cmp == 0) {
+                return true;
+            } else if (cmp < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
