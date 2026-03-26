c1d757da46fde4abe3ccc9396f2e6717c80071c0
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-26 16:35:34 +0900

    [Silver II] Title: 예산, Time: 148 ms, Memory: 15336 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -2,57 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, chong;
+	static int N, M;
 	static int[] arr;
+	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int sum = 0;
-		int max = 0;
-		
 		arr = new int[N];
-		for(int i =0; i<N; i++) {
+		int max = 1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			sum += arr[i];
 			max = Math.max(max, arr[i]);
 		}
-		Arrays.sort(arr);
 		
-		chong = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 		
+		//인덱스 가 아니고 값이다!
+		int left = 0;//0으로 나누기 에러가 날 확률은?
+		int right = max;
 		int ans = 0;
-		if(chong>=sum) {
-			System.out.println(max);
-		}else {
-			int left = 0;
-			int right = arr[N-1];
+		while(left<=right) {
+			int mid = (left + right)/2;
 			
-			while(left<=right) {
-				int mid = (left+right)/2;
-				
-				if(canMid(mid)<=chong) {
-					ans = mid;
-					left = mid+1;
-				}else if(canMid(mid)>chong) {
-					right = mid-1;
-				}
-			}
-			System.out.println(ans);
-		}
-	}
-	static int canMid(int target) {
-		int summ = 0;
-		for(int i =0; i<N; i++) {
-			if(arr[i]<=target) {
-				summ+= arr[i];
+			if(canSpend(mid) <= M) {
+
+				ans = mid;// 굳이 math.max를 안써도 자동으로 max값이 들어갈껄..? left가 점점 증가하니까.
+				left = mid+1;
 			}else {
-				summ+=target;
+				right = mid-1;
 			}
 		}
 		
-		return summ;
+		System.out.println(ans);
+	}
+    static int canSpend(int target) {
+        int sum = 0;
+		for(int i = 0; i< N; i++) {
+			if(arr[i]>= target) {
+				sum+= target;
+			}else {
+				sum += arr[i];
+			}
+		}
+		return sum;
 	}
 }
