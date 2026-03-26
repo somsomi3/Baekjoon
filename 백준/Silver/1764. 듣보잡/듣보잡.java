722fe4de5fcd4dd1c6f45afaf7e4300c32548e54
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-26 17:22:47 +0900

    [Silver IV] Title: 듣보잡, Time: 296 ms, Memory: 26252 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String[] Ns;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,21 +9,18 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Ns = new String[N];
+        Set<String> set = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            Ns[i] = br.readLine();
+            set.add(br.readLine());
         }
-
-        Arrays.sort(Ns); // 문자열 정렬
 
         List<String> result = new ArrayList<>();
 
         for (int i = 0; i < M; i++) {
-            String target = br.readLine();
-
-            if (binarySearch(target)) {
-                result.add(target);
+            String s = br.readLine();
+            if (set.contains(s)) {
+                result.add(s);
             }
         }
 
@@ -38,25 +33,5 @@ public class Main {
         }
 
         System.out.print(sb);
-    }
-
-    static boolean binarySearch(String target) {
-        int left = 0;
-        int right = Ns.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            int cmp = Ns[mid].compareTo(target);
-
-            if (cmp == 0) {
-                return true;
-            } else if (cmp < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
     }
 }
