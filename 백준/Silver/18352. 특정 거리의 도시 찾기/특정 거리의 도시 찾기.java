e9b79eafc3bd488547f7f936cd35c1061cb159da
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-27 16:10:35 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1208 ms, Memory: 270608 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,11 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	//거리가 1이라서 단방향이고 bfs랑 dfs쓰면 될거같은데, 지금 나의 머리에는 다익스트라뿐..
+	//그래서 Edge는 안만드는 다익스트라를 해보자.
 	static int N, M, K, X;
-	
 	static List<Integer>[] graph;
+	//최단 거리이기 때문에 bfs말고 다익으로 풀거라서 visited없이 dist만 만들자
 	static int[] dist;
-	static boolean[] visited;
+	static int INF = Integer.MAX_VALUE;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,51 +17,52 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());//시작점
 		
 		graph = new ArrayList[N+1];
-		for(int i = 1; i<=N; i++)graph[i]= new ArrayList<>();
-		
-		visited = new boolean[N+1];
+		for(int i = 1; i<= N; i++)graph[i] = new ArrayList<>();
 		dist = new int[N+1];
-//		Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist, -1);
+		Arrays.fill(dist, INF);
 		
-		for(int i = 0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i = 0; i< M; i++) {
+			st= new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
+			//단방향
 			graph[a].add(b);
 		}
-
+		
 		dijkstra(X);
 		
-		boolean flag = false;
-		for(int i = 1; i <= N; i++) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1; i<=N; i++) {
 			if(dist[i]==K) {
-				System.out.println(i);
-				flag = true;
+//				sb.append(dist[i]).append('\n');
+				sb.append(i).append('\n');
 			}
 		}
-		if(!flag)System.out.println(-1);
-	}
-	
-	static void dijkstra(int start) {
-//		PriorityQueue<Integer>[] pq = new PriorityQueue<>();
-		Queue<Integer> q = new LinkedList<>();
-		dist[start] = 0;
-		q.add(start);
+        if (sb.length() == 0) System.out.println(-1);
+        else System.out.println(sb);
 		
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-
-            for(int next : graph[cur]) {
-                if(dist[next] == -1) {
-                    dist[next] = dist[cur] + 1;
-                    q.add(next);
-                }
-            }
-        }
-    }
+	}
+	static void dijkstra(int start) {
+		//pq필요없음. 다 가중치가 1이다.
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(start);
+		dist[start] = 0;
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			for(int next: graph[cur]) {
+				if(dist[next]== INF) {
+					dist[next] = dist[cur]+1;
+					q.offer(next);
+				}
+			}
+			
+		}
+	}
 }
