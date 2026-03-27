24b25b5ccfd3c99ead1a421628acb2e9e81516d0
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 00:16:57 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1188 ms, Memory: 85468 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,28 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
 	static List<Integer>[] graph;
 	static boolean[] visited;
-//	static int[] parent;
-	static int[] subtree;
+	static int[] dist;
+	static int[] child;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
+		int N= Integer.parseInt(st.nextToken());
+		
+		//graph visited dist child
+		graph = new ArrayList[N+1];
+		for(int i=1; i<=N; i++)graph[i] = new ArrayList<>(); //그래프의 인덱스는 1부터 시작한다.
+		visited = new boolean[N+1];
+		dist = new int[N+1];
+		child = new int[N+1];
+		
 		int R = Integer.parseInt(st.nextToken());
 		int Q = Integer.parseInt(st.nextToken());
 		
-		//그래프만들기
-		graph = new ArrayList[N+1];
-		for(int i =1; i<=N; i++)graph[i] = new ArrayList<>();
-		
-		visited = new boolean[N+1];
-		subtree = new int[N+1];
-		
-		for(int i =0; i<N-1; i++) {
+		//가중치와 방향성이 없음.
+		for(int i=0; i<N-1; i++) {//트리의 간선은 항상 N-1개이다.
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
@@ -31,30 +32,25 @@ public class Main {
 			graph[u].add(v);
 			graph[v].add(u);
 		}
+		dfs(R);// 루트에서 시작한다.
 		
-		dfs(R);
-		//for(int i= 0; i<Q; i++) {
-		//	int x  = Integer.parseInt(br.readLine());
-		//	System.out.println(subtree[x]);
-		//}
-        // 최적화
-		StringBuilder sb = new StringBuilder();
-		for(int i =0; i<Q; i++) {
+		
+		for(int i= 0; i<Q; i++) {
 			int x = Integer.parseInt(br.readLine());
-			sb.append(subtree[x]).append('\n');
+			System.out.println(child[x]);
 		}
-        System.out.print(sb);
 	}
 	static void dfs(int cur) {
 		visited[cur] = true;
-		//
-	    subtree[cur] = 1;
+		child[cur] = 1;// 이걸 dfs(R)호출전에 넣고 호출해야 하나? 
+		//왜냐면 항상 초기화 하면 안되잖아..? 아.. 항상 초기화 해야 각각의 nxt값만 더하는듯.
 		
-		for(int nxt : graph[cur]) {
+		for(int nxt: graph[cur]) {
 			if(!visited[nxt]) {
-//				parent[cur]++;
+				//nxt들 다돌고 와서
 				dfs(nxt);
-				subtree[cur] += subtree[nxt];//자식 값을 더해준다.
+				//child[cur]에 child[nxt]를 더해주자.
+				child[cur] +=child[nxt];
 			}
 		}
 	}
