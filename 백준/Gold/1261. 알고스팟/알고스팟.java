867adcdb393dd5ae392fc7860a46ed906bee1acc
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-27 19:08:16 +0900

    [Gold IV] Title: 알고스팟, Time: 112 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,63 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int x, y, cost;
-        Node(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
+    static int N, M;
+    static int[][] arr;
+    static int[][] dist;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        dist = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        bfs();
+        System.out.println(dist[N - 1][M - 1]);
+    }
+    static void bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{0, 0});
+        dist[0][0] = 0;
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.pollFirst();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                int cost = dist[x][y] + arr[nx][ny];
+
+                if (dist[nx][ny] > cost) {
+                    dist[nx][ny] = cost;
+
+                    if (arr[nx][ny] == 0) dq.addFirst(new int[]{nx, ny});
+                    else dq.addLast(new int[]{nx, ny});
+                }
+            }
         }
     }
-    
-    static int[][] graph;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-	static int[][] dist;
-    static int N, M;
-    
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		M= Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-        graph = new int[N][M];
-        dist = new int[N][M];
-		for(int i= 0; i<N; i++) {
-			 String line = br.readLine();
-			for(int j= 0; j<M; j++) {
-				graph[i][j] = line.charAt(j) - '0';
-				dist[i][j] = Integer.MAX_VALUE;
-			}
-		}
-		dijkstra();
-		
-		System.out.println(dist[N-1][M-1]);
-		
-	}
-	static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost-b.cost);
-		dist[0][0] =0; //시작점
-		pq.add(new Node(0, 0, 0));
-		
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
-			
-			if(cur.cost > dist[cur.x][cur.y]) continue;
-			
-			for(int d =0; d<4; d++) {
-				int nx = cur.x+dx[d];
-				int ny = cur.y+dy[d];
-				
-				if(nx<0|| ny<0||nx>=N||ny>=M)continue;
-				
-				if(dist[cur.x][cur.y]+graph[nx][ny]<dist[nx][ny]) {
-					dist[nx][ny] = dist[cur.x][cur.y]+graph[nx][ny];
-					pq.add(new Node(nx, ny, dist[cur.x][cur.y]+graph[nx][ny]));
-				}
-			}
-		}
-	}
 }
