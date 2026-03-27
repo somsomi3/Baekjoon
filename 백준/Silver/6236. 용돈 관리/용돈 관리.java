f95270335f061e5949277b4ca55a7629ccf9187e
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-27 20:33:54 +0900

    [Silver I] Title: 용돈 관리, Time: 244 ms, Memory: 22524 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -2,12 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+	static int N, M; 
 	static int[] arr;
-	static int N, M;
-	static int sum=0;
-	
-	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,40 +12,46 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		
 		arr = new int[N];
-		for(int i =0; i<N; i++) {
-			arr[i]= Integer.parseInt(br.readLine());
-			sum += arr[i];
-		}
-		
 		int max = 0;
-		//최솟값주의 
-		for (int x : arr) max = Math.max(max, x);
-		int left = max;
-		int right = sum;
-		
-		int ans =1_000_000_001;
-		while(left<=right) {
-			int mid = (left+right)/2;
-			
-			if(canCount(mid)<=M) {
-				right= mid-1;
-				ans = Math.min(mid,ans);
-			}else left =mid+1;
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			max = Math.max(max, arr[i]);
 		}
 		
-		System.out.println(ans);
-	}
-	static int canCount(int mid) {
-		int count =1; 
-		int a = mid;
-		for(int i=0; i<N; i++) {
-			if(arr[i]<=a) {
-				a-= arr[i];
-			}else if(arr[i]>a) {
-				count++;
-			    a = mid - arr[i];
+		
+		int left = max;
+//		int right = 10000;
+		int right = 0;
+		for(int i = 0; i < N; i++) {
+		    right += arr[i];
+		}
+		int ans = 0;
+		while(left<= right) {
+			int mid = (left + right) /2;
+			
+			if(canSpend(mid)<=M) {
+				ans = mid;
+				right = mid -1;
+			}else {
+				left = mid +1;
 			}
 		}
-		return count;
+		System.out.println(ans);
+		
+	}
+	static int canSpend(int target) {
+		
+		int x = target;
+		int cnt = 1;
+		for(int i = 0; i<N; i++) {
+			if(x >= arr[i]) {
+				x -= arr[i];
+			}else {
+                cnt++;
+                x = target - arr[i];
+			}
+				
+		}
+		return cnt;
 	}
 }
