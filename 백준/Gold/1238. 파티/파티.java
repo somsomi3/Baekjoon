1cc6a985a7d7e8bd4311eae2a934b5d843175356
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-27 17:23:15 +0900

    [Gold III] Title: 파티, Time: 192 ms, Memory: 18644 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -4,20 +4,16 @@ import java.util.*;
 public class Main {
 	static class Edge{
 		int to, cost;
-		
 		Edge(int to, int cost){
-			
 			this.to = to;
 			this.cost = cost;
 		}
 	}
-	
 	static int N, M, X;
 	static List<Edge>[] graph;
-	static List<Edge>[] rgraph;
-	
-	static boolean[] visited;
-	static int[] dist; 
+	static List<Edge>[] reverseGraph;
+	static int[] dist;
+	static int[] reverseDist;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,68 +24,62 @@ public class Main {
 		X = Integer.parseInt(st.nextToken());
 		
 		graph = new ArrayList[N+1];
-		for(int i =1; i<=N; i++)graph[i] = new ArrayList<>();
-		
-		rgraph = new ArrayList[N+1];
-		for(int i =1; i<=N; i++)rgraph[i] = new ArrayList<>();
-		
+		reverseGraph = new ArrayList[N+1];
+		for(int i = 1; i<=N; i++) {
+			graph[i] = new ArrayList<>();
+			reverseGraph[i] = new ArrayList<>();
+		}
 //		dist = new int[N+1];
-//		Arrays.fill(dist, Integer.MAX_VALUE);
-		visited = new boolean[N+1];
+		reverseDist = new int[N+1];
 		
-		for(int i =0; i<M; i++) {
+		for(int i = 0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
 			
-			//진짜
+			//단방향이다. 그래프 두개 만들어서 넣어라.
 			graph[from].add(new Edge(to, cost));
+			reverseGraph[to].add(new Edge(from, cost));
 			
-			//역 그래프
-			rgraph[to].add(new Edge(from, cost));
 		}
+		//다익스트라 값을 배열안에 넣어주기
+		int[] go = dijkstra(X, graph);        // X → i
+		int[] back = dijkstra(X, reverseGraph); // i → X
 		
-		//진짜방향
-		int[] go = dijkstra(graph, X);
-		int[] back = dijkstra(rgraph, X);
-		
-        int answer = 0;
+		int max = 0;
 
-        for (int i = 1; i <= N; i++) {
-            answer = Math.max(answer, go[i] + back[i]);
-        }
+		for (int i = 1; i <= N; i++) {
+		    max = Math.max(max, go[i] + back[i]);
+		}
 
-        System.out.println(answer);
-    }
+		System.out.println(max);
 	
-	static int[] dijkstra(List<Edge>[] g, int start) {
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-		PriorityQueue<Edge> pq = new PriorityQueue<>(
-				(a, b)-> a.cost-b.cost
-		);
+	}
+	static int[] dijkstra(int start, List<Edge>[] g) {//타입주의
+	    int[] dist = new int[N+1];
+	    Arrays.fill(dist, Integer.MAX_VALUE);
+	    
+		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2)-> o1.cost-o2.cost);
 		dist[start] = 0;
-        pq.offer(new Edge(start, 0));
-        
-        while(!pq.isEmpty()) {
-        	Edge cur = pq.poll();
-        	if(cur.cost>dist[cur.to])continue;
-        	//dist[cur.to]: 현재까지 모은 최단의 cost들.
-        	
-        	for(Edge next: g[cur.to]) {
-//        		if(dist[next.to] > cur.cost + next.cost) {
-//        			dist[next.to] = cur.cost + next.cost;
-        		if(dist[next.to] > dist[cur.to] + next.cost) {
-        			dist[next.to] = dist[cur.to]+ next.cost;
-        			pq.offer(new Edge(next.to, dist[next.to]));
-        		}
-        	}
-        	
-        }
-        
-        
-        return dist;
+		pq.offer(new Edge(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			
+//			if(cur.to < cur.cost)continue;
+			if(cur.cost > dist[cur.to]) continue;
+			
+			for(Edge next : g[cur.to]) {
+				int nextCost = cur.cost + next.cost;
+				if(dist[next.to]> nextCost) {
+					dist[next.to]= nextCost;
+					pq.offer(new Edge(next.to, nextCost));
+				}
+			}
+		}
+		return dist;
+		
+		
 	}
 }
