ad8b21be01483d0578e8e34f4011cece92c0955a
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 20:30:43 +0900

    [Gold V] Title: 집합의 표현, Time: 412 ms, Memory: 51676 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,53 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int[] parent;
-
 	static void union(int a, int b) {
-	    int rootA = find(a);
-	    int rootB = find(b);
-
-	    if (rootA != rootB) {
-	        parent[rootB] = rootA;
-	    }
+		int rootA  = find(a);
+		int rootB  = find(b);
+		if(rootA!= rootB) {
+			parent[rootB]= rootA; 
+		}
 	}
 	static int find(int x) {
-	    if (parent[x] == x) return x;
-	    return parent[x] = find(parent[x]); // 경로 압축
+		if(parent[x]==x)return x;
+		return parent[x] = find(parent[x]);
 	}
 	
+	static int N, M;
+	static int[] parent;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		parent = new int[N+1];
+		StringBuilder sb = new StringBuilder();
 		
-		for (int i = 0; i <= N; i++)
-		    parent[i] = i;
-
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-
-            int command = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if (command == 0) {
-                union(a, b);
-            } else {
-                if (find(a) == find(b)) {
-                    sb.append("YES\n");
-                } else {
-                    sb.append("NO\n");
-                }
-            }
-        }
-
-        System.out.print(sb);
-    }
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		parent = new int[N+1];
+		for(int i = 0; i<= N; i++)parent[i]=i;//일단 부모는 자기자신
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			if(a==0) {
+				if(find(b)!=find(c)) {
+					union(b, c);
+				}
+			}else {
+				if(find(b)!= find(c))sb.append("NO").append("\n");
+				else sb.append("YES").append("\n");
+			}
+		}
+		System.out.println(sb);
+		
+	}
 }
