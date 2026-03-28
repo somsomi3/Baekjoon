4eaf3b41c507e3b7489af213867bbb2e0cf93802
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 19:29:32 +0900

    [Gold IV] Title: 미로만들기, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -2,64 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Node{
-		int x, y, cost;
-		Node(int x, int y, int cost){
-			this.x = x;
-			this.y = y;
-			this.cost =cost;
-		}
-	}
 	
-	static int[][] graph;
-	static int[][] dist;
 	static int N;
 	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {1, -1, 0, 0};
+	static int[] dy = {-1, 1, 0, 0};
+	static int[][] arr;
+	static boolean[][] visited;
+	static int[][] dist;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		
-		//그래프와 dist초기화
-		graph = new int[N][N];
+		arr = new int[N][N];
+		visited = new boolean[N][N];
 		dist = new int[N][N];
 		
-		for(int i=0; i<N; i++) {
-			String line = br.readLine();
-			for(int j =0; j<N; j++) {
-				if (line.charAt(j) == '1') graph[i][j] = 0; // 흰 방
-				else graph[i][j] = 1;                      // 검은 방
-				dist[i][j] =Integer.MAX_VALUE;
+		for(int i = 0; i<N; i++) {
+			String s= br.readLine();
+			for(int j = 0; j<N; j++) {
+				arr[i][j] = s.charAt(j) -'0';
+				dist[i][j] = Integer.MAX_VALUE;
 			}
 		}
 		
-		dijkstra();
+		bfs(0, 0);
 		
 		System.out.println(dist[N-1][N-1]);
-		
 	}
-	static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b)-> a.cost -b.cost);
-		dist[0][0] =0;
-		pq.add(new Node(0, 0, 0));
+	static void bfs(int x, int y) {
+		Deque<int[]> dq = new ArrayDeque<>();
+		dq.offer(new int[] {x, y});
+		dist[x][y] = 0;
 		
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
+		while(!dq.isEmpty()) {
+			int[] cur = dq.poll();
+			int cx = cur[0];
+			int cy = cur[1];
 			
-			if(cur.cost > dist[cur.x][cur.y]) continue;
-			
-			for(int i=0; i<4; i++) {
-				int nx = cur.x+dx[i];
-				int ny = cur.y+dy[i];
+
+			for(int i = 0; i<4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
 				
-				if(nx<0||ny<0||nx>=N||ny>=N)continue;
-				if(dist[cur.x][cur.y]+graph[nx][ny]<dist[nx][ny]) {
-					dist[nx][ny] = dist[cur.x][cur.y]+graph[nx][ny];
-					pq.add(new Node(nx, ny, dist[cur.x][cur.y]+graph[nx][ny]));
-				}
-			}	
+				if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+				
+//				if(arr[nx][ny]==1)dq.addLast(dq.pollFirst());
+				
+				if(arr[nx][ny] == 1) {//흰방
+				    if(dist[nx][ny] > dist[cx][cy]) {
+				        dist[nx][ny] = dist[cx][cy];
+				        dq.addFirst(new int[]{nx, ny});
+				    }
+				} else {//검은방
+				    if(dist[nx][ny] > dist[cx][cy] + 1) {
+				        dist[nx][ny] = dist[cx][cy] + 1;
+				        dq.addLast(new int[]{nx, ny});
+				    }
+				}	
+				
+			}
 		}
 	}
+	
 }
