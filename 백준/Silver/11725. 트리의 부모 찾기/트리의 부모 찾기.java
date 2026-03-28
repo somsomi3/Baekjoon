2428d00e4cceb5ff2a0b0daee8e329efb0c7a0e8
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 22:59:46 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 532 ms, Memory: 57296 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,48 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] tree;
-    static int[] parent;
-    static boolean[] visited;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        tree = new ArrayList[N + 1];
-        parent = new int[N + 1];
-        visited = new boolean[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            tree[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < N - 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            tree[a].add(b);
-            tree[b].add(a);
-        }
-
-        dfs(1);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 2; i <= N; i++) {
-            sb.append(parent[i]).append("\n");
-        }
-        System.out.print(sb);
-    }
-
-    static void dfs(int node) {
-        visited[node] = true;
-
-        for (int next : tree[node]) {
-            if (!visited[next]) {
-                parent[next] = node;
-                dfs(next);
-            }
-        }
-    }
+	static int N;
+	static List<Integer>[] graph;
+	static boolean[] visited;
+	static int[] parent;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		graph = new ArrayList[N+1];
+		for(int i = 1 ; i<= N; i++)graph[i] = new ArrayList<>();
+		parent = new int[N+1];
+		visited = new boolean[N+1];
+		
+		for(int i = 0; i<N-1 ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(b);
+			graph[b].add(a);
+			
+		}
+		
+		lca(1);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i =2; i<=N; i++) {
+			sb.append(parent[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+	static void lca(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		visited[start] = true;
+		q.offer(start);
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			
+			for(int next: graph[cur]) {
+				if(!visited[next]) {
+					visited[next]= true;
+					parent[next] = cur;
+					q.offer(next);
+				}
+			}
+		}
+		
+	}
 }
