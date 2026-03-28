8a49fedd884b583a98484be08f96412d6e3f1fc5
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 14:37:59 +0900

    [Bronze II] Title: 공약수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -2,49 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int g = 0;
+
+        if(n == 2) {
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            g = gcd(a, b);
+        } else {
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            g = gcd(gcd(a, b), c);
+        }
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i * i <= g; i++) {
+            if(g % i == 0) {
+                list.add(i);
+                if(i != g / i) list.add(g / i);
+            }
+        }
+
+        Collections.sort(list);
+
+        for(int x : list) {
+            sb.append(x).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 	static int gcd(int a, int b) {
-		while(b>0) {
-			int r = a%b;
-			a=b;
-			b=r;
-		}
-		return a;
-	}
-	
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		
-		int g= arr[0];
-		for(int i =1; i<N; i++) {
-			g = gcd(g, arr[i]);
-		}
-//		for(int j=1; j<=g; j++) {
-//		if(g%j==0) {
-//			System.out.println(j);
-//			}
-//		}
-		List<Integer> res = new ArrayList<>();
-
-		for (int i = 1; i * i <= g; i++) {
-		    if (g % i == 0) {
-		        res.add(i);
-		        if (i != g / i) {
-		            res.add(g / i);
-		        }
-		    }
-		}
-
-		Collections.sort(res);
-		for (int x : res) {
-		    System.out.println(x);
-		}
+	    while (b != 0) {
+	        int temp = a % b;
+	        a = b;
+	        b = temp;
+	    }
+	    return a;
 	}
 }
