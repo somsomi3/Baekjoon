805b7fd7612b87a292fb774c77726ff8be2cd347
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 17:28:40 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 140 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -2,38 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		Deque<int[]> dq = new ArrayDeque<>();
 
-        Deque<int[]> dq = new ArrayDeque<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= N; i++) {
-            dq.addLast(new int[]{i, Integer.parseInt(st.nextToken())});
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        while (!dq.isEmpty()) {
-            int[] cur = dq.pollFirst();
-            
-            sb.append(cur[0]).append(" ");
-            int move = cur[1];
-
-            if (dq.isEmpty()) break;
-
-            if (move > 0) {
-                for (int i = 0; i < move - 1; i++) {
-                    dq.addLast(dq.pollFirst());
-                }
-            } else {
-                for (int i = 0; i < -move; i++) {
-                    dq.addFirst(dq.pollLast());
-                }
-            }
-        }
-
-        System.out.println(sb.toString().trim());
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			dq.offer(new int[]{Integer.parseInt(st.nextToken()), i+1});
+		}
+		
+		while(dq.size()>1) {
+			int[] x = dq.poll();
+			sb.append(x[1]).append(" ");
+			
+			if(x[0]> 0) {
+				for(int i = 0; i<x[0]-1; i++) {
+					dq.addLast(dq.pollFirst());	
+				}
+			}else {
+				for(int i = 0; i<-x[0]; i++) {
+					dq.addFirst(dq.pollLast());
+				}
+			}
+		}
+		int[] last = dq.poll();
+		sb.append(last[1]);
+		
+		System.out.println(sb);
+		
+		
+	}
 }
