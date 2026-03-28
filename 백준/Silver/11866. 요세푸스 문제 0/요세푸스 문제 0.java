e90b28f7905e67c4638eff4d6a10c7ee40df9db2
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 16:53:51 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 132 ms, Memory: 15656 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,52 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] queue;
-    static int front = 0;
-    static int rear = 0;
-    static int size = 0;
-    static int capacity;
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		
+		for(int i = 1; i<=N; i++) {
+			dq.offer(i);
+		}
+		
+		while(dq.size()>1) {
+			for(int i = 0; i< K-1; i++) {
+				dq.addLast(dq.pollFirst());
+			}
+			sb.append(dq.pollFirst()).append(", ");
+		}
+		sb.append(dq.pollFirst()).append(">");
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]);
-
-        capacity = N;
-        queue = new int[N];
-
-        for (int i = 1; i <= N; i++) {
-            enqueue(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-
-        while (size > 0) {
-            for (int i = 0; i < K - 1; i++) {
-                enqueue(dequeue());  // 앞에서 꺼내 뒤로 보냄
-            }
-            sb.append(dequeue());
-            if (size > 0) sb.append(", ");
-        }
-
-        sb.append(">");
-        System.out.println(sb);
-    }
-
-    static void enqueue(int x) {
-        queue[rear] = x;
-        rear = (rear + 1) % capacity;
-        size++;
-    }
-
-    static int dequeue() {
-        int val = queue[front];
-        front = (front + 1) % capacity;
-        size--;
-        return val;
-    }
+		System.out.println(sb);	
+	}
 }
