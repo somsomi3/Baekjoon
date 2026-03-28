b383d17ead54c159c370fb2fedae71637e292a52
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 10:00:41 +0900

    [Silver II] Title: 과자 나눠주기, Time: 600 ms, Memory: 115128 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -2,8 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int right, N, M;
+	static int M, N; 
 	static int[] arr;
+	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,35 +12,41 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1];
-		
-		st = new StringTokenizer(br.readLine());//줄내에서 띄어쓰기
-		int right = 0;
-		for(int i =0; i<N; i++) {
+		arr= new int[N];
+		int max = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			right = Math.max(right, arr[i]);
+			max = Math.max(max, arr[i]);
 		}
 		
-		int left =1;
-		int ans =0;
-
-		while(left<= right) {
-			int mid = (left+right)/2;
-			if(bigbig(mid)) {
+//		int left = 0;
+		//과자를 조개서 나눠줄수도 있으므로
+		int left = 1;
+		
+		//right = 최대 = 가장 긴 과자의 길이
+		int right = max;
+		int ans = 0;
+		
+		while(left <= right) {
+			int mid = (left + right) /2;
+			
+			if(canCount(mid)>=M) {
 				ans = mid;
 				left = mid+1;
-				
 			}else {
-				right =mid-1;
+				right = mid -1;
 			}
+			
 		}
-        System.out.println(ans);
+		System.out.println(ans);
+		
 	}
-
-	static boolean bigbig(int stick) {
-        if (stick == 0) return true; // 방어코드
-        int count = 0;
-        for (int snack : arr) count += snack / stick;
-        return count >= M;
+	static int canCount(int target) {
+		int cnt = 0;
+		for(int i = 0; i< N; i++) {
+			if(arr[i]>=target)cnt += arr[i] / target;
+		}
+		return cnt;
 	}
 }
