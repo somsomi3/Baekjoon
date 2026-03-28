3d0a8f170453eb725278a19ca92326669bb2659f
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 16:37:42 +0900

    [Silver III] Title: 회전하는 큐, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -2,45 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken()); // 큐 크기
-        int M = Integer.parseInt(st.nextToken()); // 뽑을 개수
-
-        LinkedList<Integer> deque = new LinkedList<>();
-        for (int i = 1; i <= N; i++) {
-            deque.add(i);
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+			
+		Deque<Integer> dq = new LinkedList<>();
+		
+        for(int i = 1; i<=N; i++) {
+        	dq.add(i);
         }
-
+        
+        int cnt = 0;
         st = new StringTokenizer(br.readLine());
-        int count = 0;
-
-        for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-
-            int idx = deque.indexOf(target);
-            int halfIdx = deque.size() / 2;
-
-            if (idx <= halfIdx) {
-                // 왼쪽 회전 (2번 연산)
-                while (deque.getFirst() != target) {
-                    deque.addLast(deque.removeFirst());
-                    count++;
-                }
-            } else {
-                // 오른쪽 회전 (3번 연산)
-                while (deque.getFirst() != target) {
-                    deque.addFirst(deque.removeLast());
-                    count++;
-                }
-            }
-
-            // 맨 앞 제거 (1번 연산, 카운트 X)
-            deque.removeFirst();
+        while(M -->0) {
+        	
+        	int x = Integer.parseInt(st.nextToken());
+        	
+        	//위치 찾기
+        	int idx = 0;
+        	for(int ind: dq) {
+        		if(ind == x) {
+        			break;
+        		}else idx++;
+        	}
+        	//위치에 따라 /2 위치 별로 분리
+        	
+        	if(idx <= dq.size()/2) {
+        		//반보다 적을때
+        		 while(dq.peekFirst() != x) {
+                     dq.addLast(dq.pollFirst());
+                     cnt++;
+                 }
+        	}else {
+        		while(dq.peekFirst()!= x) {
+        			dq.addFirst(dq.pollLast());
+        			cnt++;
+        		}
+        		
+        	}
+        	dq.pollFirst();
         }
-
-        System.out.println(count);
-    }
+        System.out.println(cnt);
+	}
 }
