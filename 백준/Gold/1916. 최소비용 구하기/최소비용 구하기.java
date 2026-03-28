c9865a07b028311a3a7f94b938e5299c8146fbcb
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 18:57:54 +0900

    [Gold V] Title: 최소비용 구하기, Time: 476 ms, Memory: 51092 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -4,15 +4,16 @@ import java.util.*;
 public class Main {
 	static class Edge{
 		int to, cost;
-		
 		Edge(int to, int cost){
 			this.to = to;
 			this.cost = cost;
 		}
 	}
-	static int N, M; 
+	
+	static int N, M, x, y;
 	static List<Edge>[] graph;
 	static int[] dist;
+	static int INF = Integer.MAX_VALUE;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,48 +22,47 @@ public class Main {
 		M = Integer.parseInt(br.readLine());
 		
 		graph = new ArrayList[N+1];
-		for(int i = 1; i<=N; i++)graph[i] = new ArrayList<>();
-		
+		for(int i =1; i <=N; i++)graph[i] = new ArrayList<>();
 		dist = new int[N+1];
-//		Arrays.fill(dist, -1);
+		Arrays.fill(dist, INF);
 		
-		for(int i = 0; i < M; i++) {
+		for(int i = 0; i< M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 			
-			//A->B단방향
-			graph[from].add(new Edge (to, cost));
+			
+			graph[a].add(new Edge(b, c));
 		}
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
 		
-		dijkstra(start);
+		dijkstra(x);
 		
-		System.out.println(dist[end]);
+		System.out.println(dist[y]!=INF? dist[y]:"INF");
+		
+		
 	}
-	
 	static void dijkstra(int start) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-	    Arrays.fill(dist, Integer.MAX_VALUE);
-	    dist[start] = 0;
-	    pq.offer(new Edge(start, 0));
-	    
-	    while (!pq.isEmpty()) {
-	        Edge cur = pq.poll();
-	        
-	        if (dist[cur.to] < cur.cost) continue;
-	        
-	        for (Edge next : graph[cur.to]) {
-	            int nextCost = dist[cur.to] + next.cost;
-	            
-	            if (dist[next.to] > nextCost) {
-	                dist[next.to] = nextCost;
-	                pq.offer(new Edge(next.to, nextCost));
-	            }
-	        }
-	    }
+		PriorityQueue<Edge>pq = new PriorityQueue<>((a1, a2)-> a1.cost - a2.cost);
+		pq.offer(new Edge(start, 0));
+		dist[start]= 0;
+		
+		while(!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			
+			if(dist[cur.to]<cur.cost)continue;
+			
+			for(Edge next: graph[cur.to]) {
+				int nextCost = cur.cost+ next.cost;
+				if(dist[next.to]>nextCost) {
+					dist[next.to]= nextCost;
+					pq.offer(new Edge(next.to, nextCost));
+				}
+			}
+		}
 	}
 }
