718b2b81ffe58fbe59d520f271282e105a5d58dc
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-28 18:24:06 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 548 ms, Memory: 68336 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static class Edge{
@@ -10,24 +10,23 @@ public class Main {
 		}
 	}
 	
-	static int N, M; 
+	static int N, E;
 	static List<Edge>[] graph;
-	static int[] dist;//각 점에 대한 dist라서 일차원 배열
-	static int INF= Integer.MAX_VALUE;
+	static int[] dist;
+	static int INF = Integer.MAX_VALUE;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken()); 
+		E = Integer.parseInt(st.nextToken());
 		
 		graph = new ArrayList[N+1];
-		for(int i = 1; i <= N; i++)graph[i] = new ArrayList<>();
-		dist = new int[N+1];
-		Arrays.fill(dist, INF);
+		for(int i = 1; i<=N; i++)graph[i] = new ArrayList<>();
+
 		
-		for(int i  = 0; i<M; i++) {
+		for(int i = 0; i< E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
@@ -35,41 +34,48 @@ public class Main {
 			
 			graph[a].add(new Edge(b, c));
 			graph[b].add(new Edge(a, c));
-			
-		}
+	
+		}	
+//		무방향= 양방향
+		
 		st = new StringTokenizer(br.readLine());
-		int X = Integer.parseInt(st.nextToken());
-		int Y = Integer.parseInt(st.nextToken());
-		
-		
-		dijkstra(1);
-		int a = dist[X];
-		int b = dist[Y];
-		
-		//초기화..?
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+
+		dist = new int[N+1];
 		Arrays.fill(dist, INF);
-		dijkstra(X);
-		int c = dist[Y];
+		dijkstra(1);
+		int a = dist[x];
+		int b = dist[y];
+		
+		
+		dist = new int[N+1];
+		Arrays.fill(dist, INF);
+		dijkstra(x);
+		int c = dist[y];
 		int d = dist[N];
 		
+		dist = new int[N+1];
 		Arrays.fill(dist, INF);
-		dijkstra(Y);
+		dijkstra(y);
 		int e = dist[N];
-		int f = dist[X];
+		int f = dist[x];
 		
-		int case1 = (a == INF || c == INF || e == INF) ? INF : a + c + e;
-		int case2 = (b == INF || f == INF || d == INF) ? INF : b + f + d;
-
-		int ans = Math.min(case1, case2);
-		
-        System.out.println(ans == INF ? -1 : ans);
-		
-		
+		boolean flag = false;
+		int min= INF;
+		if(a==INF||c==INF||e==INF)flag= false;
+		else if(b==INF||f==INF||d==INF)flag= false;
+		else {
+			min = Math.min(a+c+e, b+f+d);
+			flag = true;
+		}
+		if(flag)System.out.println(min);
+		else System.out.println(-1);
 	}
 	static void dijkstra(int start) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2)-> o1.cost - o2.cost);
 		pq.offer(new Edge(start, 0));
-		dist[start] = 0;
+		dist[start] =0;
 		
 		while(!pq.isEmpty()) {
 			Edge cur = pq.poll();
@@ -78,13 +84,11 @@ public class Main {
 			
 			for(Edge next: graph[cur.to]) {
 				int nextCost = cur.cost+ next.cost;
-				
-				if(dist[next.to]>nextCost) {//이걸 위해서 dist를 -1로 채우면 안됨
-					dist[next.to] = nextCost;
+				if(dist[next.to]>nextCost) {
+					dist[next.to]= nextCost;
 					pq.offer(new Edge(next.to, nextCost));
 				}
 			}
-			
 		}
 	}
-}
+}	
