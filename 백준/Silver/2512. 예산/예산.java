d21a098ced5ae53e36dc236503937c33e431e4f3
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-29 18:00:13 +0900

    [Silver II] Title: 예산, Time: 144 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -6,47 +6,48 @@ public class Main {
 	static int[] arr;
 	
 	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		
-		arr = new int[N];
-		int max = 1;
+		arr= new int[N];
+		int max = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			max = Math.max(max, arr[i]);
 		}
+		//배열 안에서 값을 찾는게 아니라서, 정렬할 필요없음.
 		
 		M = Integer.parseInt(br.readLine());
 		
-		//인덱스 가 아니고 값이다!
-		int left = 0;//0으로 나누기 에러가 날 확률은?
+		int left = 1;
 		int right = max;
 		int ans = 0;
-		while(left<=right) {
-			int mid = (left + right)/2;
+		while(left <= right) {
+			int mid = left +(right - left) /2;
 			
-			if(canSpend(mid) <= M) {
-
-				ans = mid;// 굳이 math.max를 안써도 자동으로 max값이 들어갈껄..? left가 점점 증가하니까.
+			if(canSum(mid)<=M) {
+				ans = mid;
 				left = mid+1;
 			}else {
-				right = mid-1;
+				right = mid -1;
+			}
+		}
+		System.out.println(ans);
+		
+	}
+	static int canSum(int target) {
+		int sum = 0;
+		
+		for(int i = 0; i< N; i++) {
+			if(arr[i]<target) {
+				sum+= arr[i];
+			}else {
+				sum+= target;
 			}
 		}
 		
-		System.out.println(ans);
-	}
-    static int canSpend(int target) {
-        int sum = 0;
-		for(int i = 0; i< N; i++) {
-			if(arr[i]>= target) {
-				sum+= target;
-			}else {
-				sum += arr[i];
-			}
-		}
 		return sum;
 	}
 }
