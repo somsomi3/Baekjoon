a0dce01e8ec6dd7130aba8f693f8120779fae6b8
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-29 19:28:27 +0900

    [Silver V] Title: 숫자 카드, Time: 1228 ms, Memory: 112340 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
+	static int N, M; 
 	static int[] arr;
 	static int[] brr;
 	
@@ -13,43 +13,43 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		
 		arr = new int[N];
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i= 0; i<N; i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
 		
 		M = Integer.parseInt(br.readLine());
-		brr = new int[M];
+		
+		brr= new int[M];
 		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<M; i++) {
+		for(int i = 0; i<M; i++) {
 			brr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 0; i<M; i++) {
+		for(int i = 0; i< M; i++) {
 			
-			//인덱스 값이다.
+			//인덱스
 			int left = 0;
-			
 			int right = N-1;
-			boolean flag = false;
 			
+			boolean flag = false;
 			while(left<= right) {
-				int mid = (left+right)/2;
-				if(arr[mid]==brr[i]) {
+				int mid = left + (right - left)/2;
+				
+				if(arr[mid]== brr[i]) {
 					flag = true;
 					break;
-				}else if( arr[mid]<brr[i]) {
-					left = mid +1;
+				}else if(arr[mid]> brr[i]) {
+					right = mid -1;
 				}else {
-					right = mid- 1;
+					left = mid +1;
 				}
 			}
-			if(flag)sb.append(1).append(" ");
-			else sb.append(0).append(" ");
+			
+			if(flag)sb.append("1").append(" ");
+			else sb.append("0").append(" ");
 		}
 		System.out.println(sb);
-		
 	}
 }
