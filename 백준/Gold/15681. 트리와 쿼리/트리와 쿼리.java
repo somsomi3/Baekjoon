fc744e3e895d71da8df97f332c30d35e1e43723c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-29 23:13:51 +0900

    [Gold V] Title: 트리와 쿼리, Time: 524 ms, Memory: 77976 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -3,10 +3,8 @@ import java.util.*;
 
 public class Main {
 	static int N, R, Q;
+	static int[] child;
 	static List<Integer>[] graph;
-	static int[] tree;
-	static int[] parent;
-	static boolean[] visited;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,55 +16,38 @@ public class Main {
 		Q = Integer.parseInt(st.nextToken());
 		
 		graph = new ArrayList[N+1];
-		for(int i = 1; i<=N; i++)graph[i] = new ArrayList<>();
+		for(int i = 1; i<=N; i++)graph[i]= new ArrayList<>();
 		
-		parent = new int[N + 1];
-		tree = new int[N+1];
-		for(int i = 1; i<= N; i++) {
-			parent[i] = i;
-			tree[i] = 1;//가중치가 다 1
-		}
-		visited = new boolean[N+1];
+		child = new int[N+1];
 		
-		for(int i = 0; i < N-1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int U = Integer.parseInt(st.nextToken());
-			int V = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<N-1; i++) {
+			st= new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 			
-			//트리는 양방향?
-			graph[U].add(V);
-			graph[V].add(U);
+			graph[u].add(v);
+			graph[v].add(u);
 			
 		}
 		
-		dfs(R);
+		dfs(R, 0);//시작점
 		
 		for(int i = 0; i<Q; i++) {
 			int x = Integer.parseInt(br.readLine());
-			sb.append(tree[x]).append("\n");
+			sb.append(child[x]).append("\n");
 		}
 		System.out.println(sb);
 		
 	}
-	//일단은 tree안쓰고 큐를 써서 해도 되지않을가. parent가 visited역할을 하니까.
-	//아냐 누적할게 없네.
-	static void dfs(int start) {
-		Queue<Integer>q = new ArrayDeque<>();
-		q.offer(start);
-		visited[start] = true;
+	static void dfs(int cur, int parent) {
+		child[cur] = 1;
 		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
+		for(int next: graph[cur]) {
 			
-			for(int next: graph[cur]) {
-				if(!visited[next]) {
-					dfs(next);
-					tree[cur]+= tree[next];
-//					q.offer(next); //=>이건 굳이 있어야할 이유가...
-				}
-			}
+			if(next ==parent)continue;
+			dfs(next, cur);
+			child[cur]+= child[next];
 		}
 		
 	}
-	
 }
