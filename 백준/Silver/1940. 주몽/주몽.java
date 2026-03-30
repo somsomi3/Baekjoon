fe951e64d39b591872a3cb646571c1d3dffc0d04
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-30 16:22:38 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16392 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,36 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        int left = 0;
-        int right = N - 1;
-        int count = 0;
-
-        while (left < right) {
-            int sum = arr[left] + arr[right];
-            if (sum == M) {
-                count++;
-                left++;
-                right--;
-            } else if (sum < M) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        System.out.println(count);
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		//인덱스 값이므로 
+		int left = 0;
+		int right = N-1;
+		int cnt = 0;
+		
+		while(left< right) {
+			int sum = arr[left]+ arr[right];
+			
+			if(sum<M) {
+				left++;
+				
+			}else if(sum==M) {
+				cnt++;
+			    left++;
+			    right--;
+			}else {
+				right--;
+			}
+		}
+		System.out.println(cnt);
+	}
 }
