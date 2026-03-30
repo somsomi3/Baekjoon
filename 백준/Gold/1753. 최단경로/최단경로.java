a34765cbebebe975155a6b685eb95c66442726c8
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-30 19:44:03 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 107712 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -18,39 +18,45 @@ public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		
 		K = Integer.parseInt(br.readLine());
 		
-		graph = new ArrayList[V+1];
-		dist = new int[V+1];
-		for(int i =1; i<= V; i++)graph[i] = new ArrayList<>();
+		graph = new ArrayList[V + 1];
+		for(int i = 1; i<=V; i++)graph[i] = new ArrayList<>();
+		dist = new int[V + 1];
+
 		Arrays.fill(dist, INF);
 		
 		for(int i = 0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
 			
-			//양방향아닌 듯.
-			graph[a].add(new Edge(b, c));
+			//방향그래프
+			graph[u].add(new Edge(v, w));
 			
 		}
 		
 		dijkstra(K);
 		
-		for(int i = 1; i<= V; i++) {
-			if(dist[i]==INF)sb.append("INF").append("\n");
-			else sb.append(dist[i]).append("\n");
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1; i<=V; i++) {
+			if(dist[i]!= INF) {
+				sb.append(dist[i]).append("\n");
+			}else {
+				sb.append("INF").append("\n");
+			}
 		}
+		
 		System.out.println(sb);
 	}
 	static void dijkstra(int start) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2)-> o1.cost - o2.cost);
 		pq.offer(new Edge(start, 0));
 		dist[start] = 0;
 		
@@ -59,13 +65,14 @@ public class Main {
 			
 			if(dist[cur.to]<cur.cost)continue;
 			
-			for(Edge next : graph[cur.to]) {
-				int nextCost = cur.cost+ next.cost;
-				if(dist[next.to]>nextCost) {
+			for(Edge next: graph[cur.to]) {
+				int nextCost = cur.cost + next.cost;
+				if(dist[next.to]> nextCost) {
 					dist[next.to] = nextCost;
 					pq.offer(new Edge(next.to, nextCost));
 				}
 			}
 		}
 	}
+	
 }
