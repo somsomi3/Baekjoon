078177481627e9506076cef14be00c95c6f403a7
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-30 11:02:31 +0900

    [Silver IV] Title: 수 찾기, Time: 588 ms, Memory: 45396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -8,43 +8,47 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[N];
+		int[] arr= new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
+		for(int i = 0;i< N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-			
 		Arrays.sort(arr);
-	
+		
 		int M = Integer.parseInt(br.readLine());
+		
+		int[] brr = new int[M];
 		st = new StringTokenizer(br.readLine());
-		int[] brr =new int[M];
 		for(int i = 0; i<M; i++) {
 			brr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		
-		for(int i = 0; i< M; i++) {
+		for(int i = 0; i<M; i++) {
 			
+			//인덱스 값
 			int left = 0;
 			int right = N-1;
+			
 			boolean flag = false;
 			while(left<= right) {
-				int mid = (left+ right)/2;
+				//==값을 찾는데
+				int mid = (left +right)/2;
 				
 				if(arr[mid]== brr[i]) {
 					flag = true;
 					break;
-				}else if(arr[mid] > brr[i]) {
+				}else if(arr[mid]>brr[i]) {
 					right = mid -1;
 				}else {
 					left = mid+1;
 				}
 				
 			}
-			if(flag)sb.append("1").append("\n");
-			else sb.append("0").append("\n");
+			if(flag)sb.append(1).append("\n");
+			else sb.append(0).append("\n");
 		}
 		System.out.println(sb);
+		
 	}
 }
