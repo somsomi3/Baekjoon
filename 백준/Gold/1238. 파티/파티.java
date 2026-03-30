775feb3703145b61bb79df8d903bc25369f19887
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-30 20:54:48 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -9,11 +9,11 @@ public class Main {
 			this.cost = cost;
 		}
 	}
+	
 	static int N, M, X;
-	static List<Edge>[] graph;
-	static List<Edge>[] reverseGraph;
+	static List<Edge>[] graph, reverseGraph;
 	static int[] dist;
-	static int[] reverseDist;
+//	static int INF = 1_000_000_000;
 	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,61 +25,51 @@ public class Main {
 		
 		graph = new ArrayList[N+1];
 		reverseGraph = new ArrayList[N+1];
-		for(int i = 1; i<=N; i++) {
+		for(int i = 1; i<= N; i++) {
 			graph[i] = new ArrayList<>();
 			reverseGraph[i] = new ArrayList<>();
 		}
-//		dist = new int[N+1];
-		reverseDist = new int[N+1];
 		
 		for(int i = 0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+			st= new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 			
-			//단방향이다. 그래프 두개 만들어서 넣어라.
-			graph[from].add(new Edge(to, cost));
-			reverseGraph[to].add(new Edge(from, cost));
-			
+			graph[s].add(new Edge(e, c));
+			reverseGraph[e].add(new Edge(s, c));
 		}
-		//다익스트라 값을 배열안에 넣어주기
-		int[] go = dijkstra(X, graph);        // X → i
-		int[] back = dijkstra(X, reverseGraph); // i → X
+		
+		int[] go = dijkstra(X, graph);
+		int[] back = dijkstra(X, reverseGraph);
 		
 		int max = 0;
-
-		for (int i = 1; i <= N; i++) {
-		    max = Math.max(max, go[i] + back[i]);
+		for(int i = 1; i<=N; i++) {
+			max = Math.max(max, go[i]+back[i]);
 		}
-
 		System.out.println(max);
-	
 	}
-	static int[] dijkstra(int start, List<Edge>[] g) {//타입주의
-	    int[] dist = new int[N+1];
-	    Arrays.fill(dist, Integer.MAX_VALUE);
-	    
-		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2)-> o1.cost-o2.cost);
-		dist[start] = 0;
+	static int[] dijkstra(int start, List<Edge>[] g) {
+		dist = new int[N+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2)-> o1.cost - o2.cost);
 		pq.offer(new Edge(start, 0));
+		dist[start] = 0;
 		
 		while(!pq.isEmpty()) {
 			Edge cur = pq.poll();
 			
-//			if(cur.to < cur.cost)continue;
-			if(cur.cost > dist[cur.to]) continue;
+			if(dist[cur.to]<cur.cost)continue;
 			
-			for(Edge next : g[cur.to]) {
-				int nextCost = cur.cost + next.cost;
-				if(dist[next.to]> nextCost) {
+			for(Edge next: g[cur.to]) {
+				int nextCost = cur.cost +next.cost;
+				if(dist[next.to]>nextCost) {
 					dist[next.to]= nextCost;
 					pq.offer(new Edge(next.to, nextCost));
 				}
 			}
-		}
+		}	
 		return dist;
-		
-		
 	}
 }
