9a05f2adc33c7cfb1683685fe3c97949e2430731
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-30 11:34:57 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 28796 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N , C;
+	static int N, C;
 	static int[] arr;
 	
 	public static void main(String[] args)throws IOException{
@@ -12,42 +12,44 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N]; 
-		for(int i = 0; i<N; i++) {
+		arr = new int[N];
+		
+		for(int i =0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
+		//정렬해라
 		Arrays.sort(arr);
+		int left = 1;
+		int right = arr[N-1]- arr[0];//최대 사이간격은 처음 좌표와 마지막 좌표의 간격이다.
 		
-		
-//		int left = arr[0];
-//		int right= arr[N-1];
-        int left = 1; // 최소 거리
-        int right = arr[N - 1] - arr[0]; // 최대 거리
 		int ans = 0;
 		
-		while(left<=right) {
-			int mid = (left+right)/2;
+		while(left <= right) {
+			//사이간격이라는 mid값을 찾는 문제이다.
+			int mid = (left+ right)/2;
 			
-			if(canInstall(mid)>=C) {
+			if(canInstall(mid)>= C) {
 				ans = mid;
-				left = mid +1;
+				left = mid + 1;
 			}else {
 				right = mid - 1;
 			}
+			
 		}
 		System.out.println(ans);
+		
 	}
 	static int canInstall(int target) {
 		int start = arr[0];
 		int cnt = 1;
 		
-		for(int i = 0; i<N; i++) {
-			if (start+target<=arr[i]) {
+		for(int i = 1; i<N; i++) {
+			if(arr[i]-start>= target) {
 				cnt++;
+				// 설치 햇을때만 갱신해야함.
 				start = arr[i];
 			}
 		}
-		
 		return cnt;
 	}
-}
+}	
