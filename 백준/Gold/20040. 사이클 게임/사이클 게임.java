a35a2547dfe653a80825260b7d7e5e0256267f96
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-31 10:25:44 +0900

    [Gold IV] Title: 사이클 게임, Time: 584 ms, Memory: 124776 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -2,46 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
-    static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-
-        if (rootA != rootB) {
-            parent[rootB] = rootA;
-        }
-    }
-
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        int answer = 0;
-
-        for (int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if (find(a) == find(b)) {
-                answer = i;
-                break;
-            }
-            union(a, b);
-        }
-
-        System.out.println(answer);
-    }
+	static void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		
+		if(rootA != rootB) {
+			parent[rootB] = rootA;
+		}
+	}
+	static int find(int x) {
+		if(parent[x]==x)return x;
+		return parent[x] = find(parent[x]);
+	}
+	
+	static int[] parent;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int result = 0;
+		boolean flag = false;
+		
+		parent = new int[n+1];
+		for(int i = 1; i<=n; i++)parent[i] = i;
+		
+		for(int i = 0; i<m; i++) {
+			st =new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(find(a)!= find(b)) {
+				union(a, b);
+				result++;
+			}else {
+				flag = true;
+				result++;
+				break;
+			}
+			
+		}
+		if(flag)System.out.println(result);
+		else System.out.println(0);
+	}
 }
