268a2335fbfe3f73a08b1b357b0be4832abbdc94
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-03-31 09:50:00 +0900

    [Gold V] Title: 집합의 표현, Time: 496 ms, Memory: 52900 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,10 +3,11 @@ import java.util.*;
 
 public class Main {
 	static void union(int a, int b) {
-		int rootA  = find(a);
-		int rootB  = find(b);
-		if(rootA!= rootB) {
-			parent[rootB]= rootA; 
+		int rootA = find(a);
+		int rootB = find(b);
+		
+		if(rootA != rootB) {
+			parent[rootB] = rootA; 
 		}
 	}
 	static int find(int x) {
@@ -14,7 +15,6 @@ public class Main {
 		return parent[x] = find(parent[x]);
 	}
 	
-	static int N, M;
 	static int[] parent;
 	
 	public static void main(String[] args)throws IOException{
@@ -22,28 +22,32 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
-		parent = new int[N+1];
-		for(int i = 0; i<= N; i++)parent[i]=i;//일단 부모는 자기자신
+		parent = new int[n+1];
+		for(int i = 1; i<=n; i++)parent[i] = i;//처음에는 자기자신을 부모로 해서 시작함.
 		
-		for(int i = 0; i < M; i++) {
+		for(int i = 0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
+			
 			if(a==0) {
-				if(find(b)!=find(c)) {
+				if(find(b)!= find(c)) {
 					union(b, c);
 				}
 			}else {
-				if(find(b)!= find(c))sb.append("NO").append("\n");
-				else sb.append("YES").append("\n");
+				if(find(b)==find(c)) {
+					sb.append("YES").append("\n");
+				}else {
+					sb.append("NO").append("\n");
+				}
 			}
 		}
+		
 		System.out.println(sb);
 		
 	}
