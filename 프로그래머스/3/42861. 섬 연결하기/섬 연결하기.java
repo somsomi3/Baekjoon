565b80cfb0cd1de7a46d746627b8faa335d6ca3b
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-04-01 20:55:38 +0900

    [level 3] Title: 섬 연결하기, Time: 4.14 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,33 +1,54 @@
+import java.io.*;
 import java.util.*;
+
 class Solution {
-    static int[] parent;
-	
-	static int find(int x) {
-		if(parent[x]==x)return x;
-		return parent[x] = find(parent[x]);
-	}
-	
-	static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if(a !=b )parent[b]=a;
-	}
-	
-        public int solution(int n, int[][] costs) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-
-        int answer = 0, edgeCount = 0;
-        for (int[] e : costs) {
-            int a = e[0], b = e[1], w = e[2];
-            if (find(a) != find(b)) {
-                union(a, b);
-                answer += w;
-                // if (++edgeCount == n - 1) break;
-            }
+    static class Edge{
+        int from, to, cost;
+        Edge(int from, int to, int cost){
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
-        return answer;
+    }
+    
+    static int[] parent;
+    static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        
+        if(rootA != rootB){
+            parent[rootB]= rootA;
+        }
+    }
+    static int find(int x){
+        if(parent[x]==x)return x;
+        return parent[x]= find(parent[x]);
+    }
+    
+    public int solution(int n, int[][] costs) {
+        int result = 0;
+        
+        parent = new int[n+1];
+        for(int i = 1; i<n; i++)parent[i] = i;//초기값설정
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b)->a.cost- b.cost);
+        
+        for(int i = 0; i<costs.length; i++){
+            int a = costs[i][0];
+            int b = costs[i][1];
+            int c = costs[i][2];
+            
+            pq.offer(new Edge(a, b, c));
+        }
+        
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            if(find(cur.from)!= find(cur.to)){
+                union(cur.from, cur.to);
+                result += cur.cost;
+            }    
+        }
+        
+        return result;
     }
 }
