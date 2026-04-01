f674f274282f32530fb8d00611b907f7b9a195dd
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-04-01 16:40:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 20828 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,52 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MAX = 100001;
-
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        if (N == K) {
-            System.out.println(0);
-            return;
-        }
-
-        //정점 자체의 visited, dist
-        boolean[] visited = new boolean[MAX];
+	static int N, K;
+	
+	static boolean[] visited;
+	static int[] dist;
+	
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[100001];
+        dist = new int[100001];
         
-        //dist[x] : N에서 x까지 걸린 최소 시간
-        int[] dist = new int[MAX];
-
-        //BFS
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(N);
-        visited[N] = true;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            if (cur == K) {
-                System.out.println(dist[cur]);
-                return;
-            }
-
-            //현재 위치에서 갈 수 있는 3가지 이동 정의
-            //방향벡터 같은 역할
-            // 최단거리 보장
-            int[] next = {cur - 1, cur + 1, cur * 2};
-
-            for (int nx : next) {
-                if (nx >= 0 && nx < MAX && !visited[nx]) {//범위 안에 있고, 방문하지 않았다면
-                    visited[nx] = true;
-                    dist[nx] = dist[cur] + 1;
-                    q.offer(nx);
-                }
-            }
-        }
-    }
-
+        
+        bfs(N);
+        
+        System.out.println(dist[K]);
+		
+		
+	}
+	static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(start);
+		
+		visited[start] = true;
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+            if (cur == K) return;
+			
+			int[] next = {cur-1, cur+1, cur*2};
+			
+			for(int daum : next) {
+				if(daum<0||daum>100000)continue;
+				if(visited[daum])continue;
+				
+				visited[daum]= true;
+				q.offer(daum);
+				dist[daum] = dist[cur]+1;
+				
+			}
+			
+		}
+		
+	}
 }
