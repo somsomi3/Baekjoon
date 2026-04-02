1396461eed00b698ad66d579906bfd69391f4c4d
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-04-02 11:46:38 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16720 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -4,37 +4,38 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[N];
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			
 		}
-		
 		Arrays.sort(arr);
-		//인덱스 값이므로 
-		int left = 0;
+		
+		int left = 0;//인덱스다.
 		int right = N-1;
 		int cnt = 0;
+		int sum = 0;
 		
-		while(left< right) {
-			int sum = arr[left]+ arr[right];
+		while(left<right) {
+			//한가지 값을 left와 right로 두번 사용할 수 는 없다.
 			
-			if(sum<M) {
-				left++;
-				
-			}else if(sum==M) {
+			sum = arr[left] + arr[right];//매번 더해봐야한다.
+			
+			if(sum==M) {
 				cnt++;
-			    left++;
-			    right--;
-			}else {
+				left++;
 				right--;
+			}else if(sum> M) {
+				right--;
+			}else {
+				left++;
 			}
+			
 		}
 		System.out.println(cnt);
 	}
