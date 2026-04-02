ae11b053a18781ff5cbc20ba9caed1a848390fff
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-04-02 09:17:17 +0900

    [Gold V] Title: 기타 레슨, Time: 304 ms, Memory: 24028 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.java
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -13,47 +13,45 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		
 		arr= new int[N];
-		int sum = 0;
 		int max = 0;
-		st= new StringTokenizer(br.readLine());
+		int sum =0;
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<N; i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
-			sum+= arr[i];
-            max = Math.max(max, arr[i]);
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, arr[i]);
+			sum += arr[i];
 		}
 		
-		//강의는 순서대로 담아야함. 그리고 배열안에서 값을 고를 필요없음.
-//		Arrays.sort(arr);
-		
-        int left = max;//mid로 나눌일은 없다.
+		int left = max;
 		int right = sum;
 		int ans = 0;
-		
-		while(left<= right) {//특점 값을 찾는다.
-			int mid = (left + right) /2;
+		while(left<= right) {
+			int mid = (left+ right)/2;
 			
-			//M개 이하!!
-			if(canFind(mid) <= M) {
-				ans = mid;
-				right = mid -1;//더작은 mid를 향해 가므로, ans에 Math.min은 필요없다.
-			}else {
-				left = mid+1;
-			}
+			if (canMake(mid) <= M) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+			
 		}
+		
 		System.out.println(ans);
 	}
-	static int canFind(int target) {
-	    int cnt = 1;
-	    int sum = 0;
-	
-	    for(int i = 0; i < N; i++) {
-	        if(sum + arr[i] > target) {
-	            cnt++;
-	            sum = arr[i];
-	        } else {
-	            sum += arr[i];
-	        }
-	    }
-	    return cnt;
+	static int canMake(int target) {
+		int s = 0;
+		int xx = 0;
+		int cnt = 1;
+		for(int i = 0; i<N; i++) {
+			s+= arr[i];
+			xx= arr[i];
+			
+			if(s>target) {
+				cnt++;
+				s = arr[i];
+			}
+        }
+		return cnt;
 	}
 }
