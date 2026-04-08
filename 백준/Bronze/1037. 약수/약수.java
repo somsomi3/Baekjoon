1ad8142ed63048d83d74732740aed24588e36582
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-04-09 00:26:36 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -2,20 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int min = 1_000_001;
-        int max = 1;
-        
-        for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            min = Math.min(min, x);
-            max = Math.max(max, x);
-        }
-        
-        System.out.println(min * max);
-    }
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N= Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i <N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+		}
+		
+		Arrays.sort(arr);
+		
+		int min = 1000001;
+		int max = 1;
+		for(int i = 0; i<N; i++) {
+			max = Math.max(max, arr[i]);
+			min = Math.min(min, arr[i]);
+		}
+		
+		long ans = min*max;
+		
+		System.out.println(ans); 
+		
+	}
 }
