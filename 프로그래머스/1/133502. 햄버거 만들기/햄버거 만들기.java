3b4f9326935ad6a8be14c3ed2e84e4d5427c9181
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-05-16 23:50:15 +0900

    [level 1] Title: 햄버거 만들기, Time: 27.02 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int x : ingredient) {
+            list.add(x);
+
+            int size = list.size();
+
+            if (size >= 4 &&
+                list.get(size - 4) == 1 &&
+                list.get(size - 3) == 2 &&
+                list.get(size - 2) == 3 &&
+                list.get(size - 1) == 1) {
+
+                answer++;
+
+                list.remove(size - 1);
+                list.remove(size - 2);
+                list.remove(size - 3);
+                list.remove(size - 4);
+            }
+        }
+
+        return answer;
+    }
+}
