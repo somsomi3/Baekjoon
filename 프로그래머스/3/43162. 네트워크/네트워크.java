f23942847ef0373d32887b972224bde298bf5d1d
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-05-29 17:21:14 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,38 +1,42 @@
+//1. union find풀이
+import java.io.*;
 import java.util.*;
 
 class Solution {
-
-    boolean[] visited;
-
-    public int solution(int n, int[][] computers) {
-
-        visited = new boolean[n];
-
-        int answer = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i, computers);
-                answer++;
-            }
-        }
-        return answer;
+    static int[] parent;
+    
+    static void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA!= rootB) parent[rootB] = rootA;
     }
-
-    void bfs(int start, int[][] computers) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            
-            for (int next = 0; next < computers.length; next++) {
-                if (computers[cur][next] == 1 && !visited[next]) {
-                    visited[next] = true;
-                    q.offer(next);
+    static int find(int x){
+        if(parent[x] ==x)return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    public int solution(int n, int[][] computers) {
+        
+        parent = new int[n];
+        
+        for(int i = 0; i< n; i++)parent[i] = i;
+        
+        for(int i = 0; i< n; i++){
+            for(int j = i+1; j <n; j++){
+                if(computers[i][j]==1){
+                    union(i, j);
                 }
             }
         }
+        
+        //같은 부모 = 같은 그룹
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i<n; i++){
+            set.add(find(i));
+        }
+        
+        int answer = set.size();
+        return answer;
     }
 }
