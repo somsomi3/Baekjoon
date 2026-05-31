3dc377cdf8a9e14c63e462357a52bac9ab03e873
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-05-31 21:58:15 +0900

    [level 1] Title: 달리기 경주, Time: 259.61 ms, Memory: 354 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < players.length; i++) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i< players.length; i++){
             map.put(players[i], i);
         }
-
-        for (String calling : callings) {
-
-            int idx = map.get(calling);
-
-            String frontPlayer = players[idx - 1];
-
-            players[idx - 1] = calling;
-            players[idx] = frontPlayer;
-
-            map.put(calling, idx - 1);
-            map.put(frontPlayer, idx);
+        
+        for(int i = 0; i< callings.length; i++){
+            
+            int x = map.get(callings[i]);
+            String front = players[x -1];
+            
+            //위치 바꾸기
+            players[x - 1] = callings[i];
+            players[x] = front;
+            
+            map.put(callings[i], map.get(callings[i])-1);
+            map.put(front, map.get(front) + 1);
+            
         }
-
+        
+        
         return players;
     }
 }
