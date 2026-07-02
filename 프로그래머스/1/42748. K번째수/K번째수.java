bf1620d1062823dd343eb84264d2bb4d0643f559
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-07-02 19:00:46 +0900

    [level 1] Title: K번째수, Time: 2.13 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,27 +3,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-
-        int[] ans = new int[commands.length];
+        int[] answer = new int[commands.length];
         
-        for(int i = 0; i< commands.length; i++){
-            
-            int start = commands[i][0] - 1;
-            int end = commands[i][1] - 1;
-            int k = commands[i][2] - 1;
-            
-            int[] answer = new int[end - start + 1];
-            
-            int idx = 0;
-            for(int j = start; j <= end; j++){
-                answer[idx++] = array[j];
+        for(int x= 0; x<commands.length; x++){
+
+
+            int i = commands[x][0];
+            int j = commands[x][1];
+            int target = commands[x][2];
+
+            int[] ranges = new int[j-i+1];
+
+            int index = 0;
+            for (int z = i - 1; z < j; z++) {
+                ranges[index] = array[z];
+                index++;
             }
             
-            Arrays.sort(answer);
-            
-            ans[i] = answer[k];      
+            Arrays.sort(ranges);
+
+            answer[x] = ranges[target-1];
         }
-        
-        return ans;
+        return answer;
     }
 }
