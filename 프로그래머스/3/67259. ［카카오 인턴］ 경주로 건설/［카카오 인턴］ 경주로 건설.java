167779a87297cfdff2237f0aa1b300473663cd1c
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-07-04 19:49:14 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2.86 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,50 +1,70 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
-    
+    static int[][][] cost;
+    static int N;
+    static int[][] map;
     public int solution(int[][] board) {
-        int n = board.length;
+        map = board;
+        N = board.length;
         
-        int[][][] cost = new int[n][n][4];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+        cost = new int[N][N][4];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 Arrays.fill(cost[i][j], Integer.MAX_VALUE);
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        for (int d = 0; d < 4; d++) {
-            cost[0][0][d] = 0;
-            q.offer(new int[]{0, 0, d, 0});
-        }
-        
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1], dir = cur[2], c = cur[3];
-            
-            for (int nd = 0; nd < 4; nd++) {
-                int nx = x + dx[nd];
-                int ny = y + dy[nd];
-                
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-                if (board[nx][ny] == 1) continue;
-                
-                int nc = c + 100;
-                if (dir != nd) nc += 500;
-                
-                if (cost[nx][ny][nd] > nc) {
-                    cost[nx][ny][nd] = nc;
-                    q.offer(new int[]{nx, ny, nd, nc});
-                }
             }
         }
         
+        bfs();
+        
         int answer = Integer.MAX_VALUE;
         for (int d = 0; d < 4; d++) {
-            answer = Math.min(answer, cost[n-1][n-1][d]);
+            answer = Math.min(answer, cost[N - 1][N - 1][d]);
         }
-        
+
         return answer;
+    
+    }
+    public void bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 1});
+        q.offer(new int[]{0, 0, 3});
+
+        cost[0][0][1] = 0;
+        cost[0][0][3] = 0;
+        
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int curDir = cur[2];
+            
+            
+            for(int d = 0; d< 4; d++){
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+                    continue;
+
+                if (map[nx][ny] == 1)
+                    continue;
+                
+                int newCost;
+
+                if (curDir == d)
+                    newCost = cost[cx][cy][curDir] + 100;
+                else
+                    newCost = cost[cx][cy][curDir] + 600;
+
+                if (newCost < cost[nx][ny][d]) {
+                    cost[nx][ny][d] = newCost;
+                    q.offer(new int[]{nx, ny, d});
+                }
+            }
+        }
     }
 }
