13d8f1c15dfcfe219973ed54085445ccfcae4906
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-07-14 20:48:28 +0900

    [level 3] Title: 입국심사, Time: 20.74 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,21 +1,39 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
-        long left = 1;
-        long right = 1_000_000_000L*n;
+        long answer = 0;
         
-        while(left<right){
-            long mid = left+(right-left)/2;
-            long total = 0;
-            for(int time: times){
-                total += mid/time;
-            }
-            if(total<n){
-            left = mid+1;
-            }else{
-            right = mid;
-            }            
+        int max = 0;
+        for(int i = 0; i< times.length; i++){
+            if(max<times[i])max = times[i];
         }
-        return left;
+        
+        //인덱스 아님 sum값임.
+        long left = 0;
+        long right = (long) max * n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (canFind(mid, n, times)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return answer;
     }
+    public boolean canFind(long mid, int n, int[] times){
+        long sum =  0;
+        
+        for(int time: times){
+            sum+= mid/time;
+            
+            if(sum >= n)return true;
+        }
+        return false;
+    }
+    
 }
