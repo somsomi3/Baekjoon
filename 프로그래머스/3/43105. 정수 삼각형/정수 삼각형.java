a567112909b74c457fdb36d14f587ed9e51755fc
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-08-06 21:53:14 +0900

    [level 3] Title: 정수 삼각형, Time: 4.23 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] triangle) {
+
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+        }
+
+        int answer = 0;
+        for (int num : triangle[triangle.length - 1]) {
+            answer = Math.max(answer, num);
+        }
+
+        return answer;
+    }
+}
