78c07c587addd0eeaf2e4eb8622968bd899517af
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-08-30 21:36:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,15 +1,20 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-
-        for(int i =0; i<signs.length; i++){
-            if( signs[i] ==false)
-                absolutes[i]= -absolutes[i];
-        }
-        int sum =0;
-        for(int i =0; i<signs.length; i++){
-            sum += absolutes[i];
+        // int answer = 123456789;//원재주어진 내용
+        int answer = 0;
+        
+        for(int i = 0; i< absolutes.length; i++){
+            if(signs[i] == true){
+                answer += absolutes[i];
+            }else{
+                answer -= absolutes[i];
+            }
+            
         }
         
-        return sum;
+        return answer;
     }
 }
