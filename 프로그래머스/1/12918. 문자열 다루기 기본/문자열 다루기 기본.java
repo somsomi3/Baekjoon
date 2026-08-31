d24339c36294828020649c363e6f2edfa3bb7600
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-08-31 21:38:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,20 +1,17 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
+        boolean answer = false;
         
-        if (s.length() != 4 && s.length() != 6) {
-            return false;
-        }
-        
-        for(int i = 0; i< s.length(); i++){
-            char x = s.charAt(i);
+        if(s.length()==4|| s.length()==6){
+            answer = true;
             
-            if(!Character.isDigit(x)){
-                answer = false;
-                break;
+            for(int i = 0; i< s.length(); i++){
+                char c = s.charAt(i);
+                
+                if(!Character.isDigit(c)){
+                    answer = false;
+                    break;
+                }
             }
         }
         
