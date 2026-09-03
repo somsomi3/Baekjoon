32f3fefe727a0c4a131f7580065794e78cc07656
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-09-03 21:25:31 +0900

    [level 1] Title: 덧칠하기, Time: 1.20 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,15 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int end =  0;
-        
-        for(int x: section){
-            if(end< x){
+        int x = 0;
+        for(int i = 0; i< section.length; i++){
+            if(x < section[i]){
+                x = section[i]+m -1;
                 answer++;
-                end = x + m -1;
             }
         }
-        
         return answer;
     }
 }
