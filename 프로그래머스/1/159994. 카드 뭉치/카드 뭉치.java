b9b5c7ec3f9e0813533a515ff4f1ff454800ba89
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-09-03 20:12:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,22 +1,49 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        // String answer = "";
-        // return answer;
-        int i = 0;
-        int j = 0;
+        String answer = "";
         
-        for(String x : goal){
-            if(i <cards1.length && x.equals(cards1[i])){
-                i++;
-             }else if (j <cards2.length && x.equals(cards2[j])) {
-                j++;
+        Queue<String> q = new ArrayDeque<>();
+        Queue<String> q1 = new ArrayDeque<>();
+        Queue<String> q2 = new ArrayDeque<>();
+        
+        for(int i = 0; i< goal.length; i++){
+            q.offer(goal[i]);
+        }
+        
+        for(int i = 0; i< cards1.length; i++){
+            q1.offer(cards1[i]);
+        }
+        
+        for(int i = 0; i< cards2.length; i++){
+            q2.offer(cards2[i]);
+        }
+        int i = q.size();
+        boolean flag = true;
+        while(i> 0){
+            // if(q.peek()== q1.peek()){
+            if(!q1.isEmpty() && q.peek().equals(q1.peek())){
+                q.poll();
+                q1.poll();
+                i--;
+            // }else if(q.peek()== q2.peek()){
+            }else if(!q2.isEmpty() && q.peek().equals(q2.peek())){
+                q.poll();
+                q2.poll();
+                i--;
             }else{
-                return "No";
+                flag = false;
+                break;
             }
         }
-        return "Yes";
+        
+        if(flag){
+            answer = "Yes";
+        }else{
+            answer = "No";
+        }
+        
+        return answer;
     }
 }
