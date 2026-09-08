b7406c6720ba5189859152e19bf81de18d9d01ab
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-09-08 22:09:44 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-                
         int[] answer = {};
-        
-        int sum = brown+ yellow;
-        int x;
-        int y;
-        
-        for(int i = 1; i<= sum; i++){
-            if(sum%i==0){
-                x = i;
-                y = sum/i;
-                if((x-2)*(y-2)== yellow){
-                    return new int[]{y, x};
-                }
+        int sum = brown + yellow;
+        for(int i = 1; i<=yellow; i++){
+            if(yellow%i ==0){
+                int x = i;
+                int y = yellow/i;
                 
+                if((x+2)*(y+2)== sum){
+                    return new int[]{y+2, x+2};
+                }
             }
         }
-
+        
         return answer;
     }
 }
