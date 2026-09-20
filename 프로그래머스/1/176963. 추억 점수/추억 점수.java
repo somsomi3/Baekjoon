c7c750d14ef5fb00529cd048f4124659c16c697a
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-09-20 21:21:30 +0900

    [level 1] Title: 추억 점수, Time: 1.29 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,19 +2,18 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+    public int[] solution(String[] name, int[] yearning, String[][] photo){
         Map<String, Integer> map = new HashMap<>();
-        
+        int[] answer = new int[photo.length]; 
         for(int i = 0; i<name.length; i++){
-            map.put(name[i], yearning[i]);    
+            map.put(name[i],yearning[i]);    
         }
-        int[] answer = new int[photo.length];
-        
+
         for(int i = 0; i<photo.length; i++){
             int sum = 0;
             for(int j = 0; j<photo[i].length; j++){
                 if(map.containsKey(photo[i][j])){
-                    sum += map.get(photo[i][j]);
+                    sum +=map.get(photo[i][j]);
                 }
             }
             answer[i]= sum;
