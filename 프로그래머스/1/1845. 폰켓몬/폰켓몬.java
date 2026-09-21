2cda2ff231c9406a115aa29ee12c26cf4969af87
somsomi3 <153436610+somsomi3@users.noreply.github.com>
2026-09-21 22:11:14 +0900

    [level 1] Title: 폰켓몬, Time: 3.99 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,23 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int n = nums.length/2;
+        
+        for(int i = 0; i < nums.length; i++){
+            // map.put(getOrdefault +1);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
         int answer = 0;
         
-        int x = nums.length/2;
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for(int i = 0; i< nums.length; i++){
-            set.add(nums[i]);
+        if(n > map.size()){
+            answer = map.size();
+        }else{
+            answer = n;
         }
-        
-        if(set.size() >= x){
-            answer = x;
-        }else answer = set.size();
         
         return answer;
     }
